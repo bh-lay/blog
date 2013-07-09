3fd8562aa0a5026a6627e119dd0269df685a83a8
剧中人 <bh_lay@126.com>
2013-07-08 19:53:29 -0700

    remove admin from web directories

--- web/admin/user/user_group.html.js
+++
@@ -1,8 +0,0 @@
-//author bh-lay
-var fs = require('fs');
-var pageTpl = fs.readFileSync('../web/admin/user/user_group.html', "utf8");
-exports.render = function (req,res){
-	res.writeHead(200, {'Content-Type': 'text/html'});
-	res.write(pageTpl);
-	res.end();
-}
