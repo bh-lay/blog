a33976c947f2ec7fe5ff1e2a22e52813c6e2d867
剧中人 <bh_lay@126.com>
2013-08-04 08:56:21 -0700

    try to parse post and get data

--- sys/lib/post.js
+++ sys/lib/post.js
@@ -13,23 +13,26 @@ exports.parse = function(req,callBack){
 		return 
 	}
 
-	var content_type = req['headers']['content-type'];
-
-	var form = new formidable.IncomingForm();
-	form.uploadDir = "./temporary";
-	//form.keepExtensions = true;
+	var method = req['method']||'';
 	
-	form.parse(req, function(error, fields, files) {
-	
-		console.log(1234,fields,files);
-		// @FIXME when i upload more than one file ,the arguments files is only single file
-		// but i can get all files information form form.openedFiles
-		// it confused me
-		//console.log(1234,arguments);
+	if(method == 'POST' || method =='post'){
+		var form = new formidable.IncomingForm();
+		form.uploadDir = "./temporary";
+		//form.keepExtensions = true;
 		
-		files = form.openedFiles;
+		form.parse(req, function(error, fields, files) {
+			// @FIXME when i upload more than one file ,the arguments files is only single file
+			// but i can get all files information form form.openedFiles
+			// it confused me
+			//console.log(1234,arguments);
+			
+			files = form.openedFiles;
+			
+			callBack(error,fields, files);
 		
-		callBack(error,fields, files);
-	
-	});
+		});
+	}else{
+		var fields = querystring.parse(req.url.split('?')[1]);
+		callBack(null,fields,[]);
+	}
 }
