253eeaa4ef90f5e4bb5812b22b8492d103f4eed0
剧中人 <bh_lay@126.com>
2013-08-12 06:12:17 -0700

    just do some upload

--- sys/templates/share.js
+++ sys/templates/share.js
@@ -41,13 +41,13 @@ function detail_page(res_this,id){
 		method.open({'collection_name':'share'},function(err,collection){
 			
 			collection.find({id:id}).toArray(function(err, docs) {
-				if(arguments[1].length==0){
+				if(docs.length==0){
 
 					res_this.notFound('哇塞，貌似这篇分享不存在哦!');
 					
 				}else{
-					var date=new Date(parseInt(docs[0].time_show));
-					docs[0].time_show=(date.getYear()+1900)+'-'+(date.getMonth()+1)+'-'+date.getDate();
+					var date = new Date(parseInt(docs[0].time_show));
+					docs[0].time_show = (date.getYear()+1900)+'-'+(date.getMonth()+1) + '-' + date.getDate();
 					var txt = page_temp.replace(/\{-(\w*)-}/g,function(){
 						return docs[0][arguments[1]]||'';
 					});
