a84ec720b12e6527a5a78257cf09ff689b25485e
剧中人 <bh_lay@126.com>
2013-09-08 02:50:16 -0700

    add share and opus ajax

--- sys/conf/ajax.js
+++ sys/conf/ajax.js
@@ -5,8 +5,10 @@
 
 module.exports = {
 	'article':'article_add&edit',
-	'share':'share_add&edit',
-	'opus':'opus_add&edit',
+	'share_edit':'share_add&edit',
+	'share':'share_get',
+	'opus_edit':'opus_add&edit',
+	'opus':'opus_get',
 	'blog':'article_get',
 	'user':'user/user_add&edit',
 	'user_group':'user/user_group_add&edit',
