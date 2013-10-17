e38774825ee3b7e849e602c34c71be11049de344
剧中人 <bh_lay@126.com>
2013-10-17 08:12:21 -0700

    edit forwarded request url

--- sys/mod/response.js
+++ sys/mod/response.js
@@ -25,59 +25,55 @@ var zlib = require("zlib");
 
 var notFoundTemp = fs.readFileSync('./templates/404.html', "utf8");
 
-//FIXME logger 
-var logger = (function(){
+//Unified use send response method
+function send (status,headers,content){
 	
+	var _this = this;
 	
-})();
+	var headers = headers || {};
+	headers['server'] = 'nodejs';
+	headers['Connection'] = 'keep-alive';
+	headers['Content-Encoding'] = 'gzip';
+	
+	this.res.writeHeader(status,headers);
+	
+	var content = content || null;
+	
+	if(content){
+		zlib.gzip(content, function(err, result) {
+			_this.res.end(result);
+		});
+	}else{
+		this.res.end();
+	}
+	// logger ////////////////////////////////////
+	var logger = {
+		'time':new Date(),
+		'ip':this.req['connection']['remoteAddress'],
+		'ip2' : this.req['headers']['x-forwarded-for'] ,
+		'url':this.req.url,
+		'user-agent':this.req.headers['user-agent'],
+		'status' : status
+	};
+	console.log(logger);
+}
 
 function connect(req,res){
 	this.req = req;
 	this.res = res;
-	var _this = this;
-	//Unified use send response method
-	this.send = function (status,headers,content){
-		
-		var headers = headers || {};
-		headers['server'] = 'nodejs';
-		headers['Connection'] = 'keep-alive';
-		headers['Content-Encoding'] = 'gzip';
-		
-		this.res.writeHeader(status,headers);
-		
-		var content = content || null;
-		
-		if(content){
-			zlib.gzip(content, function(err, result) {
-				_this.res.end(result);
-			});
-		}else{
-			this.res.end();
-		}
-		// logger ////////////////////////////////////
-		var logger = {
-			'time':new Date(),
-			'ip':this.req['connection']['remoteAddress'],
-			'ip2' : req['headers']['x-forwarded-for'] ,
-			'url':this.req.url,
-			'user-agent':this.req.headers['user-agent'],
-			'status' : status
-		};
-		console.log(logger);
-	}
 }
 
 connect.prototype = {
 	//response json data
 	'json' : function(data){
-		this.send(200,{
+		send.call(this,200,{
 			'Content-Type' : 'application/json',
 			'charset' : 'utf-8',
 		},JSON.stringify(data));
 	},
 	//response html page
 	'html' : function(status,content){
-		this.send(status,{
+		send.call(this,status,{
 			'Content-Type' : 'text/html',
 			'charset' : 'utf-8',
 		},content);
@@ -85,7 +81,7 @@ connect.prototype = {
 	},
 	//response define data
 	'define' : function(status,headers,content){
-		this.send(status,headers,content);
+		send.call(this,status,headers,content);
 	},
 	//response not found
 	'notFound' : function(txt){
@@ -93,7 +89,7 @@ connect.prototype = {
 		
 		var content = notFoundTemp.replace(/{-content-}/,txt);
 
-		this.send(404,{
+		send.call(this,404,{
 			'Content-Type' : 'text/html'
 		},content);
 	},
