9dc922f3ce26aa93553539199cc532c112a8be06
剧中人 <bh_lay@126.com>
2013-10-22 22:35:05 -0700

    Limit the cache maximum number

--- sys/mod/cache.js
+++ sys/mod/cache.js
@@ -2,9 +2,11 @@
  * @author bh-lay
  */
 var fs = require('fs');
+var cache_max_num = 1000;
 
 function cache(cache_name,callback,create_cache,root){
-	var cache_path = './cache/' + root + '/' + cache_name + '.txt';
+	var cache_root = './cache/' + root + '/';
+	var cache_path = cache_root + cache_name + '.txt';
 	
 	fs.exists(cache_path, function(exists) {
 		if(exists){
@@ -27,6 +29,13 @@ function cache(cache_name,callback,create_cache,root){
 					};
 				});
 			});
+			//try to clear cache
+			fs.readdir(cache_root,function(err,files){
+				var total = files.length;
+				if(total > cache_max_num){
+					clear_directory(cache_root);
+				}
+			});
 		}
 	});
 };
@@ -45,16 +54,6 @@ function clear_directory(root_path,callback){
 	});
 }
 
-exports.chip = function(cache_name,callback,create_cache){
-	cache(cache_name,callback,create_cache,'chip');
-};
-exports.html = function(cache_name,callback,create_cache){
-	cache(cache_name,callback,create_cache,'html');
-};
-exports.ajax = function(cache_name,callback,create_cache){
-	var this_name = cache_name.replace(/\/|\?/g,'_'); 
-	cache(this_name,callback,create_cache,'ajax');
-};
 
 /**
  * cache.clear(root,name,fn)
@@ -82,23 +81,25 @@ exports.clear = function(){
 				callback&&callback(err);
 			});
 		}else{
-			clear_directory(root_path,callback)
+			clear_directory(root_path,callback);
 		}
 	}else{
 		callback&&callback('arguments[0] error please use [chip|html|ajax]');
 	}
 }
+// use cache exports
+exports.chip = function(cache_name,callback,create_cache){
+	cache(cache_name,callback,create_cache,'chip');
+};
+exports.html = function(cache_name,callback,create_cache){
+	cache(cache_name,callback,create_cache,'html');
+};
+exports.ajax = function(cache_name,callback,create_cache){
+	var this_name = cache_name.replace(/\/|\?/g,'_'); 
+	cache(this_name,callback,create_cache,'ajax');
+};
 
-//FIXME write clear cache event
-//var events = require('events');
-//var emitter = new events.EventEmitter();
-//emitter.on('clear_cache',function(type,name,callback){
-//	console.log('clear cache start',arguments);
-//});
 
-//emitter.emit('clear_cache','ajax','--a',function(){
-//	do something
-//})
 
 /*////////////////////////////////////////////////////
 @demo
