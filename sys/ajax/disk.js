f6b79c8fb5f3e48eb27da08df273756270ad4b4a
剧中人 <bh_lay@126.com>
2013-10-31 05:10:13 -0700

    修改文件目录读取功能

--- sys/ajax/disk.js
+++ sys/ajax/disk.js
@@ -3,11 +3,62 @@
  */
 var fs = require('fs');
 
+function getDir(root,callback){
+	var res = [];
+	fs.readdir(root,function(err,files){
+		if(err){
+			callback(err);
+			return
+		}
+		files.forEach(function(file){
+			var pathname = root + '/' + file,
+				stat = fs.lstatSync(pathname);
+	
+			if (stat.isDirectory()){
+				res.push({
+					'name' : file,
+					'isdir' : true
+				});
+			} else {
+				res.push({
+					'name' : file,
+					'isdir' : false
+				});
+			}
+		});
+		callback(null,res)
+	});
+}
+
+function filter_path(root){
+	if(root){
+		root = root.replace(/\.\.\//g,'/');
+		root = root.replace(/^\//,'');
+		return root;
+	}else{
+		return ''
+	}
+}
+
+var root = 'D:/open/';
 exports.render = function (req,res_this,path){
 	if(path.pathnode.length == 2){
-		fs.readdir('D:/备份/ad',function(err,files){
-			res_this.json([err,files]);
+		parse.request(req,function(err,data){
+			var path = filter_path(data.path);
+			
+			getDir(root + path,function(err,files){
+				var json = {
+					'code' : 1,
+					'files' : files
+				}
+				if(err){
+					json.code = 404;
+					json.msg = 'Directory does not exist!'
+				}
+				res_this.json(json);
+			});
 		});
+	
 	}else{
 		res_this.json({
 			'code' : 2,
