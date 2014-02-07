430509fff1b499cee28ab169903757f8a7e051ff
剧中人 <bh_lay@126.com>
2014-02-06 16:12:07 -0800

    发布过年期间关于个人后台的页面修改

---
+++ web/skin/admin/publish.js
@@ -0,0 +1,393 @@
+/*
+ * 作者:剧中人
+ * 博客：http://bh-lay.com/
+ * Copyright (c) 2012-2018 小剧客栈
+**/
+
+window.admin = window.admin || {};
+window.admin.publish = window.admin.publish || {};
+
+(function(exports){
+	var publish_tpl = ['<div class="publish">',
+		'<div class="publish_cpt">',
+			'<a href="javascript:void(0)" data-type="article" class="active">博文</a>',
+			'<a href="javascript:void(0)" data-type="share">分享</a>',
+			'<a href="javascript:void(0)" data-type="opus">作品</a>',
+			'<a href="javascript:void(0)" data-type="friends">友情链接</a>',
+		'</div>',
+		'<div class="publish_cnt"></div>',
+	'</div>'].join('');
+	function show_module (dom,cpt_dom,name,id){
+		if(name == 'share'){
+			admin.publish.share(dom,id);
+		}else if(name == 'opus'){
+			admin.publish.opus(dom,id);
+		}else if(name == 'friends'){
+			admin.publish.friends(dom,id);
+		}else{
+			//默认为发布文章
+			admin.publish.article(dom,id);
+		}
+	
+		cpt_dom.find('a').removeClass('active');
+		cpt_dom.find('a[data-type="' + name + '"]').addClass('active');
+	
+	}
+	exports.init = function(dom,param){
+		dom.html(publish_tpl);
+		var cpt_dom = dom.find('.publish_cpt');
+		var cnt_dom = dom.find('.publish_cnt');
+		
+		var param = param || {};
+		var id = param.id || null;
+		var active = param.active || 'article';
+		show_module(cnt_dom,cpt_dom,active,id);
+		cpt_dom.on('click','a',function(){
+			var name = $(this).attr('data-type');
+			show_module(cnt_dom,cpt_dom,name,null);
+			admin.push('publish/' + name);
+		});
+	};
+})(window.admin.publish);
+
+(function(exports){
+	var article_tpl = ['<div class="pub_article"><form action="/ajax/add_edit" method="post" target="_self">',
+		'<div class="pub_arti_title"><input type="text" placeholder="博文标题，必须要填的哦！" name="title" value="{title}"/></div>',
+		'<div class="pub_arti_intro">',
+			'<textarea placeholder="一段话概括博文" name="intro" cols="50" rows="5">{intro}</textarea>',
+		'</div>',
+		'<div class="pub_arti_main">',
+			'<textarea placeholder="博文正文" name="content">{content}</textarea>',
+		'</div>',
+		'<div>',
+			'<input type="text" placeholder="缩略图" name="cover" value="{cover}" />',
+			'<input type="text" placeholder="标签" name="tags" value="{tags}" />',
+			'<input type="text" placeholder="作者" name="author" value="{author}" />',
+			'<input type="text" placeholder="发表时间" name="time_show" value="{time_show}" />',
+		'</div>',
+		'<div class="pub_sub">',
+			'<input type="hidden" name="id" value="{id}" />',
+			'<input type="hidden" name="category" value="blog" />',
+			'<input type="submit" value="提交" />',
+		'</div>',
+	'</form></div>'].join('');
+	var share_tpl = ['<div class="pub_share">',
+		'<form action="/ajax/add_edit" method="post" target="_self">',
+			'<div class="pub_sha_title">',
+				'<input type="text" placeholder="分享主题" name="title" value="{title}"/>',
+			'</div>',
+			'<div class="pub_sha_intro">',
+				'<textarea name="intro" placeholder="分享简介" cols="50" rows="5">{intro}</textarea>',
+			'</div>',
+			'<div class="pub_sha_main">',
+				'<textarea name="content" palceholder="分享详情" cols="50" rows="10" >{content}</textarea>',
+			'</div>',
+			'<div>',
+				'<input type="text" placeholder="缩略图" name="cover" value="{cover}" />',
+				'<input type="text" placeholder="标签" name="tags" value="{tags}" />',
+				'<input type="text" placeholder="发表时间" name="time_show" value="{time_show}" />',
+				'<input type="text" placeholder="分享来自" name="from" value="{from}" />',
+				'<input type="text" placeholder="分享地址" name="from_url" value="{from_url}" />',
+			'</div>',
+			'<div>',
+				'<input type="hidden" name="id" value="{id}" />',
+				'<input type="hidden" name="category" value="share" />',
+				'<input type="submit" value="提交" />',
+			'</div>',
+		'</form>',
+	'</div>'].join('');
+	var opus_tpl = ['<div class="L_formUI">',
+		'<form action="/ajax/add_edit" method="post" target="_self"><ul>',
+			'<li class="L_foUItem"><label class="L_foUItTitle">标题：</label><input type="text" name="title" value="{title}"/></li>',
+			'<li class="L_foUItem"><label class="L_foUItTitle">缩略图：</label><input type="text" name="cover" value="{cover}"` /></li>',
+			'<li class="L_foUItem"><label class="L_foUItTitle">作品大图：</label><input type="text" name="opus_pic" value="{opus_pic}" /></li>',
+			'<li class="L_foUItem"><label class="L_foUItTitle">标签：</label><input type="text" name="tags" value="{tags}" /></li>',
+			'<li class="L_foUItem"><label class="L_foUItTitle">创作时间：</label><input type="text" name="opus_time_create" value="{opus_time_create}" /></li>',
+			'<li class="L_foUItem"><label class="L_foUItTitle">开发范围：</label><input type="text" name="work_range" value="{work_range}" /></li>',
+			'<li class="L_foUItem"><label class="L_foUItTitle">在线地址：</label><input type="text" name="online_url" value="{online_url}" /></li>',
+			'<li class="L_foUItem">',
+				'<label class="L_foUItTitle">作品简介：</label>',
+				'<textarea name="intro" cols="50" rows="5">{intro}</textarea>',
+			'</li>',
+			'<li class="L_foUItem">',
+				'<label class="L_foUItTitle">作品详细：</label>',
+				'<textarea name="content" cols="50" rows="10" >{content}</textarea>',
+			'</li>',
+			'<li class="L_foUItem">',
+				'<input type="hidden" name="id" value="{id}" />',
+				'<input type="hidden" name="category" value="opus" />',
+				'<input type="submit" value="提交" />',
+			'</li>',
+		'</ul></form>',
+	'</div>'].join('');
+	
+	var friend_tpl = ['<div class="L_formUI">',
+		'<form action="/ajax/add_edit" method="post" target="_self"><ul>',
+			'<li class="L_foUItem"><label class="L_foUItTitle">标题：</label><input type="text" name="title" value="{title}"/></li>',
+			'<li class="L_foUItem"><label class="L_foUItTitle">地址：</label><input type="text" name="url" value="{url}" /></li>',
+			'<li class="L_foUItem"><label class="L_foUItTitle">描述：</label><input type="text" name="discription" value="{discription}" /></li>',
+			'<li class="L_foUItem"><label class="L_foUItTitle">是否显示：</label><input type="text" name="isShow" value="{isShow}" /></li>',
+			'<li class="L_foUItem">',
+				'<input type="hidden" name="id" value="{id}" />',
+				'<input type="hidden" name="category" value="blog_friend" />',
+				'<input type="submit" value="提交" />',
+			'</li>',
+		'</ul></form>',
+	'</div>'].join('');
+	
+	/****
+	 * 获取博文内容
+	 */
+	function getArticle(id,callback){
+		if(!id){
+			callback && callback('missing arguments');
+		}
+		$.ajax({
+			'url' : '/ajax/blog',
+			'type' : 'GET',
+			'data' : {
+				'act' : 'get_detail',
+				'id' : id
+			},
+			'success' : function(data){
+				if(data.code != 1){
+					callback && callback('data error');
+				}else{
+					callback && callback(null,data.detail);
+				}	
+			}
+		});
+	}
+
+	/****
+	 * 获取分享内容
+	 */
+	function getShare(id,callback){
+		if(!id){
+			callback && callback('missing arguments');
+		}
+		$.ajax({
+			'url' : '/ajax/share',
+			'type' : 'GET',
+			'data' : {
+				'act' : 'get_detail',
+				'id' : id
+			},
+			'success' : function(data){
+				if(data.code != 1){
+					callback && callback('data error');
+				}else{
+					callback && callback(null,data.detail);
+				}	
+			}
+		});
+	}
+	
+	
+	/****
+	 * 获取作品内容
+	 */
+	function getOpus(id,callback){
+		if(!id){
+			callback && callback('missing arguments');
+		}
+		$.ajax({
+			'url' : '/ajax/opus',
+			'type' : 'GET',
+			'data' : {
+				'act' : 'get_detail',
+				'id' : id
+			},
+			'success' : function(data){
+				if(data.code != 1){
+					callback && callback('data error');
+				}else{
+					callback && callback(null,data.detail);
+				}	
+			}
+		});
+	}
+	
+	/****
+	 * 获取友情链接内容
+	 */
+	function getFriend(id,callback){
+		if(!id){
+			callback && callback('missing arguments');
+		}
+		$.ajax({
+			'url' : '/ajax/friends',
+			'type' : 'GET',
+			'data' : {
+				'act' : 'get_detail',
+				'id' : id
+			},
+			'success' : function(data){
+				if(data.code != 1){
+					callback && callback('data error');
+				}else{
+					callback && callback(null,data.detail);
+				}	
+			}
+		});
+	}
+	function valueInit(tpl,data){
+		var txt = tpl.replace(/\{(\w*)}/g,function(){
+			return data[arguments[1]]||'';
+		});
+		return txt;
+	}
+	//发布帖子
+	function ARTICLE(dom,id){
+		if(!id){
+			var new_html = valueInit(article_tpl,{});
+			dom.html(new_html);
+			admin.formToAjax(dom,{
+				'onSubmit' : function(data){
+					UI.prompt('正在发布！');
+				},
+				'onResponse' : function(data){
+					UI.prompt('博文发布完毕');
+					admin.push('/admin/');
+					admin.refresh();
+				}
+			});
+			return
+		}
+		getArticle(id,function(err,data){
+			if(err){
+				dom.html('数据异常！');
+				return
+			}
+			var new_html = valueInit(article_tpl,data);
+			dom.html(new_html);
+			admin.formToAjax(dom,{
+				'onSubmit' : function(data){
+					UI.prompt('正在提交博文修改！');
+				},
+				'onResponse' : function(data){
+					UI.prompt('博文修改完毕');
+					admin.push('/admin/');
+					admin.refresh();
+				}
+			});
+		});
+	}
+	//发布分享
+	function SHARE(dom,id){
+		if(!id){
+			var new_html = valueInit(share_tpl,{});
+			
+			dom.html(new_html);
+			admin.formToAjax(dom,{
+				'onSubmit' : function(data){
+					UI.prompt('正在提交分享修改！');
+				},
+				'onResponse' : function(data){
+					UI.prompt('分享发布完毕');
+					admin.push('/admin/');
+					admin.refresh();
+				}
+			});
+			return
+		}
+		getShare(id,function(err,data){
+			if(err){
+				dom.html('数据异常！');
+				return
+			}
+			var new_html = valueInit(share_tpl,data);
+			
+			dom.html(new_html);
+			admin.formToAjax(dom,{
+				'onSubmit' : function(data){
+					UI.prompt('正在提交分享修改！');
+				},
+				'onResponse' : function(data){
+					UI.prompt('分享修改完毕');
+					admin.push('/admin/');
+					admin.refresh();
+				}
+			});
+		});
+	}
+	function OPUS(dom,id){
+		if(!id){
+			var new_html = valueInit(opus_tpl,{});
+			
+			dom.html(new_html);
+			admin.formToAjax(dom,{
+				'onSubmit' : function(data){
+					UI.prompt('正在提交分享修改！');
+				},
+				'onResponse' : function(data){
+					UI.prompt('分享发布完毕');
+					admin.push('/admin/');
+					admin.refresh();
+				}
+			});
+			return
+		}
+		getOpus(id,function(err,data){
+			if(err){
+				dom.html('数据异常！');
+				return
+			}
+			var new_html = valueInit(opus_tpl,data);
+			
+			dom.html(new_html);
+			admin.formToAjax(dom,{
+				'onSubmit' : function(data){
+					UI.prompt('正在提交作品修改！');
+				},
+				'onResponse' : function(data){
+					UI.prompt('作品修改完毕');
+					admin.push('/admin/');
+					admin.refresh();
+				}
+			});
+		});
+	}
+	
+	//增加、修改友情链接
+	function FRIENDS(dom,id){
+		if(!id){
+			var new_html = valueInit(friend_tpl,{});
+			dom.html(new_html);
+			admin.formToAjax(dom,{
+				'onSubmit' : function(data){
+					UI.prompt('正在发布！');
+				},
+				'onResponse' : function(data){
+					UI.prompt('链接发布完毕');
+					admin.push('/admin/');
+					admin.refresh();
+				}
+			});
+			return
+		}
+		getFriend(id,function(err,data){
+			if(err){
+				dom.html('数据异常！');
+				return
+			}
+			var new_html = valueInit(friend_tpl,data);
+			dom.html(new_html);
+			admin.formToAjax(dom,{
+				'onSubmit' : function(data){
+					UI.prompt('正在提交链接修改！');
+				},
+				'onResponse' : function(data){
+					UI.prompt('链接修改完毕');
+					admin.push('/admin/');
+					admin.refresh();
+				}
+			});
+		});
+	}
+	//对外接口
+	exports.article = ARTICLE;
+	exports.share = SHARE;
+	exports.opus = OPUS;
+	exports.friends = FRIENDS;
+})(window.admin.publish);
---
+++ web/skin/admin/render.js
@@ -0,0 +1,429 @@
+/*
+ * 作者:剧中人
+ * 博客：http://bh-lay.com/
+ * Copyright (c) 2012-2018 小剧客栈
+**/
+
+window.admin = window.admin || {};
+window.admin.render = window.admin.render || {};
+
+/**
+ * 首页
+ **/
+(function(exports){
+	var txt = ['<div>',
+		'<a href="/admin/publish/article">写博文</a>--',
+		'<a href="/admin/publish/share">发分享</a>--',
+		'<a href="/admin/publish/opus">传作品</a>--',
+		'<a href="/admin/publish/friends">加友情链接</a>',
+	'</div>',
+	'<div class="fast_link">',
+		'<a href="/ajax/clear_cache?type=all">清除<strong>所有</strong>缓存</a>--',
+		'<a href="/ajax/clear_cache?type=chip">清除片段缓存</a>--',
+		'<a href="/ajax/clear_cache?type=ajax">清除ajax缓存</a>--',
+		'<a href="/ajax/clear_cache?type=html">清除页面缓存</a>--',
+		'<a href="/admin/friends">友情链接</a>',
+	'</div>'].join('');
+	function INDEX(dom){
+		dom.html(txt);
+	}
+	exports.index = INDEX;
+})(window.admin.render);
+
+/**
+ * 博文列表页
+ * 博文内容页
+ * @param {Object} dom
+ * @param {String|Number} [id] article ID
+ **/
+(function(exports){
+	var tpl = ['<tr>',
+		'<td class="arLiTitle"><a title="查看博文" href="/blog/{id}" target="_blank">{title}</a></td>',
+		'<td class="arLiEdit">',
+			'<a class="lofox" title="修改" href="/admin/publish/article/{id}" target="_self">改</a>',
+			'<a title="删除" href="/ajax/del?from=blog&id={id}" onclick="if(!confirm(\'三思啊，删了可就没啦！\')){return false;}" 	target="_self">删</a>',
+		'</td>',
+		'<td class="arLiTime">{time_show}</td>',
+	'</tr>'].join('');
+	function render(tpl,data){
+		var txt = '';
+		for(var i=0 in data){
+			txt += tpl.replace(/{(\w*)}/g,function(){
+				var key = arguments[1];
+				return data[i][key] || '';
+			});
+		}
+		return txt;
+	}
+	//获取文章列表
+	function getList(start,limit,callback){
+		$.ajax({
+			'url' : '/ajax/blog',
+			'type' : 'GET',
+			'data' : {
+				'act' : 'get_list',
+				'skip' : start,
+				'limit' : limit
+			},
+			'success' : function(data){
+				for(var i in data.list){
+					data.list[i].time_show = parse.time(data.list[i].time_show,'{y}-{m}-{d}');
+				}
+				callback(null,data);	
+			}
+		});
+	}
+	function listPage(dom){
+		var list_html = '<table class="listSheet articleList" cellspacing="0">';
+		//每页显示条数
+		var page_list_num = 8;
+		getList(0,page_list_num,function(err,data){
+			if(err){
+				console.log('error');
+				return
+			}
+			list_html += render(tpl,data.list);
+			list_html += '</table>';
+			
+			list_html += '<div class="page"></div>';
+			dom.html(list_html);
+			//分页组件
+			var page = admin.pageList(dom.find('.page'),{
+				'list_count' : data.count,
+				'page_cur' : 0,
+				'page_list_num' : page_list_num
+			});
+			page.jump = function(num){
+				
+				//console.log(num,12);
+				getList((num-1)*page_list_num,page_list_num,function(err,data){
+					if(err){
+						console.log('error');
+						return
+					}
+					var new_html = render(tpl,data.list);
+					
+					dom.find('table').html(new_html);
+				});
+			};
+		});
+	}
+	exports.article = function(dom,id){
+		listPage(dom);
+	};
+})(window.admin.render);
+
+
+/**
+ * 分享列表页
+ * 分享内容页
+ * @param {Object} dom
+ * @param {String|Number} [id] article ID
+ **/
+(function(exports){
+	var add_edit_tpl = ['<div class="L_formUI">',
+		'<form action="/ajax/add_edit" method="post" target="_self"><ul>',
+			'<li class="L_foUItem"><label class="L_foUItTitle">标题：</label><input type="text" name="title" value="{title}"/></li>',
+			'<li class="L_foUItem"><label class="L_foUItTitle">缩略图：</label><input type="text" name="cover" value="{cover}"` /></li>',
+			'<li class="L_foUItem"><label class="L_foUItTitle">标签：</label><input type="text" name="tags" value="{tags}" /></li>',
+			'<li class="L_foUItem"><label class="L_foUItTitle">发表时间：</label><input type="text" name="time_show" value="{time_show}" /></li>',
+			'<li class="L_foUItem"><label class="L_foUItTitle">分享来自：</label><input type="text" name="from" value="{from}" /></li>',
+			'<li class="L_foUItem"><label class="L_foUItTitle">分享地址：</label><input type="text" name="from_url" value="{from_url}" /></li>',
+			'<li class="L_foUItem">',
+				'<label class="L_foUItTitle">文章简介：</label>',
+				'<textarea name="intro" cols="50" rows="5">{intro}</textarea>',
+			'</li>',
+			'<li class="L_foUItem">',
+				'<label class="L_foUItTitle">正文：</label>',
+				'<textarea name="content" cols="50" rows="10" >{content}</textarea>',
+			'</li>',
+			'<li class="L_foUItem">',
+				'<input type="hidden" name="id" value="{id}" />',
+				'<input type="hidden" name="category" value="share" />',
+				'<input type="submit" value="提交" />',
+			'</li>',
+		'</ul></form>',
+	'</div>'].join('');
+	
+	var list_tpl = ['<tr>',
+		'<td class="arLiTitle"><a title="查看分享" href="/share/{id}" target="_blank">{title}</a></td>',
+		'<td class="arLiEdit">',
+			'<a title="修改" href="/admin/publish/share/{id}" target="_self">改</a>',
+			'<a title="删除" href="/ajax/del?from=share&id={id}" onclick="if(!confirm(\'三思啊，删了可就没啦！\')){return false;}" target="_self">删</a>',
+		'</td>',
+		'<td class="arLiTime">{time_show}</td>',
+	'</tr>'].join('');
+	
+	function render(tpl,data){
+		var txt = '';
+		for(var i=0 in data){
+			txt += tpl.replace(/{(\w*)}/g,function(){
+				var key = arguments[1];
+				return data[i][key] || '';
+			});
+		}
+		return txt;
+	}
+	function getList(start,limit,callback){
+		$.ajax({
+			'url' : '/ajax/share',
+			'type' : 'GET',
+			'data' : {
+				'act' : 'get_list',
+				'skip' : start,
+				'limit' : limit
+			},
+			'success' : function(data){
+				for(var i in data.list){
+					data.list[i].time_show = parse.time(data.list[i].time_show,'{y}-{m}-{d}');
+				}
+				callback(null,data);
+			}
+		});
+	}
+	function listPage(dom){
+		var list_html = '<table class="listSheet articleList" cellspacing="0">'
+				//每页显示条数
+		var page_list_num = 10;
+		getList(0,page_list_num,function(err,data){
+			if(err){
+				console.log('error');
+				return
+			}
+			list_html += render(list_tpl,data.list);
+			list_html += '</table>';
+			
+			list_html += '<div class="page"></div>';
+			dom.html(list_html);
+			var page = admin.pageList(dom.find('.page'),{
+				'list_count' : data.count,
+				'page_cur' : 0,
+				'page_list_num' : page_list_num
+			});
+			page.jump = function(num){
+				//console.log(num,12);
+				getList((num-1)*page_list_num,page_list_num,function(err,data){
+					if(err){
+						console.log('error');
+						return
+					}
+					var new_html = render(list_tpl,data.list);
+					
+					dom.find('table').html(new_html);
+				});
+			};
+		});
+	}
+	exports.share = function(dom,id){
+		//列表页
+		listPage(dom);
+	};
+})(window.admin.render);
+
+
+
+/**
+ * 作品列表页
+ * 作品内容页
+ * @param {Object} dom
+ * @param {String|Number} [id] article ID
+ **/
+(function(exports){
+	var list_tpl = ['<tr>',
+		'<td class="arLiTitle"><a title="查看作品" href="/share/{id}" target="_blank">{title}</a></td>',
+		'<td class="arLiEdit">',
+			'<a title="修改" href="/admin/publish/opus/{id}" target="_self">改</a>',
+			'<a title="删除" href="/ajax/del?from=opus&id={id}" onclick="if(!confirm(\'三思啊，删了可就没啦！\')){return false;}" target="_self">删</a>',
+		'</td>',
+		'<td class="arLiTime">{time_show}</td>',
+	'</tr>'].join('');
+	
+	function render(tpl,data){
+		var txt = '';
+		for(var i=0 in data){
+			txt += tpl.replace(/{(\w*)}/g,function(){
+				var key = arguments[1];
+				return data[i][key] || '';
+			});
+		}
+		return txt;
+	}
+	function getList(start,limit,callback){
+		$.ajax({
+			'url' : '/ajax/opus',
+			'type' : 'GET',
+			'data' : {
+				'act' : 'get_list',
+				'skip' : start,
+				'limit' : limit
+			},
+			'success' : function(data){
+				for(var i in data.list){
+					data.list[i].time_show = parse.time(data.list[i].opus_time_create,'{y}-{m}-{d}');
+				}
+				callback(null,data);
+			}
+		});
+	}
+	function OPUS(dom){
+		var list_html = '<table class="listSheet articleList" cellspacing="0">'
+				//每页显示条数
+		var page_list_num = 10;
+		getList(0,page_list_num,function(err,data){
+			if(err){
+				console.log('error');
+				return
+			}
+			list_html += render(list_tpl,data.list);
+			list_html += '</table>';
+			
+			list_html += '<div class="page"></div>';
+			dom.html(list_html);
+			var page = admin.pageList(dom.find('.page'),{
+				'list_count' : data.count,
+				'page_cur' : 0,
+				'page_list_num' : page_list_num
+			});
+			page.jump = function(num){
+				//console.log(num,12);
+				getList((num-1)*page_list_num,page_list_num,function(err,data){
+					if(err){
+						console.log('error');
+						return
+					}
+					var new_html = render(list_tpl,data.list);
+					
+					dom.find('table').html(new_html);
+				});
+			};
+		});
+	}
+	exports.opus = OPUS;
+})(window.admin.render);
+
+
+/**
+(function(exports){	
+})(window.admin.render);
+**/
+
+/***
+ * 导航
+ **/
+(function(exports){
+	function navigation (){
+		var Btn = $('.nav li a');
+		Btn.click(function(){
+			var offset = $(this).offset();
+			var cnt = $(this).next();
+			if(cnt.length == 0){
+				return
+			}
+			var plane = UI.plane({
+				'top' : offset.top + 40,
+				'left' : offset.left,
+				'width' : cnt.find('a').length * 100, 
+				'html' : cnt[0].outerHTML
+			});
+			plane.dom.click(function(){
+				plane.close();
+			});
+		});
+	}
+
+	exports.nav = function(){
+		navigation();
+		$('.userCnt').click(function(){
+			$('.username_hover').slideDown(200);
+		});
+		$('.username_hover').mouseleave(function(){
+			$(this).fadeOut(200);
+		})
+	};
+})(window.admin.render);
+
+
+/**
+ * 友情链接页面
+ * @param {Object} dom
+ * @param {String|Number} [id] article ID
+ **/
+(function(exports){
+	var tpl = ['<tr>',
+		'<td class="arLiTitle" title="博名">{title}</td>',
+		'<td class="arLiTitle" title="网址">{url}</td>',
+		'<td class="arLiTitle" title="是否显示">{isShow}</td>',
+		'<td class="arLiTitle" title="描述">{discription}</td>',
+		'<td class="arLiTitle" title="添加时间">{time_create}</td>',
+		'<td class="arLiEdit">',
+			'<a title="修改" href="/admin/publish/friends/{id}" target="_self">改</a>',
+			'<a title="删除" href="/ajax/del?from=blog_friend&id={id}" onclick="if(!confirm(\'三思啊，删了可就没啦！\')){return false;}" target="_self">删</a>',
+		'</td>',
+	'</tr>'].join('');
+	function render(tpl,data){
+		var txt = '';
+		for(var i=0 in data){
+			txt += tpl.replace(/{(\w*)}/g,function(){
+				var key = arguments[1];
+				return data[i][key] || '';
+			});
+		}
+		return txt;
+	}
+	//获取友情链接列表
+	function getList(start,limit,callback){
+		$.ajax({
+			'url' : '/ajax/friends',
+			'type' : 'GET',
+			'data' : {
+				'act' : 'get_list',
+				'skip' : start,
+				'limit' : limit
+			},
+			'success' : function(data){
+				for(var i in data.list){
+					data.list[i].time_create = parse.time(data.list[i].time_create,'{y}-{m}-{d}');
+				}
+				callback(null,data);	
+			}
+		});
+	}
+	function listPage(dom){
+		var list_html = '<table class="listSheet articleList" cellspacing="0">';
+		//每页显示条数
+		var page_list_num = 8;
+		getList(0,page_list_num,function(err,data){
+			if(err){
+				console.log('error');
+				return
+			}
+			list_html += render(tpl,data.list);
+			list_html += '</table>';
+			
+			list_html += '<div class="page"></div>';
+			dom.html(list_html);
+			//分页组件
+			var page = admin.pageList(dom.find('.page'),{
+				'list_count' : data.count,
+				'page_cur' : 0,
+				'page_list_num' : page_list_num
+			});
+			page.jump = function(num){
+				
+				//console.log(num,12);
+				getList((num-1)*page_list_num,page_list_num,function(err,data){
+					if(err){
+						console.log('error');
+						return
+					}
+					var new_html = render(tpl,data.list);
+					
+					dom.find('table').html(new_html);
+				});
+			};
+		});
+	}
+	exports.friends = function(dom,id){
+		listPage(dom);
+	};
+})(window.admin.render);
