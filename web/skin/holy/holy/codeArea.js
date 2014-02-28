506acdb2361e85508c3a330955982ddf23b991f9
bh-lay <bh_lay@126.com>
2014-02-28 11:12:21 +0800

    try to work with another one \!

--- web/skin/holy/holy/codeArea.js
+++ web/skin/holy/holy/codeArea.js
@@ -6,8 +6,6 @@ http://www.vilic.info/
 ?2010 Groinup Studio
 All rights reserved.
 
-stupid!!!!!
-
 Redistribution and use in source and binary forms,
 with or without modification, are permitted provided
 that the following conditions are met:
