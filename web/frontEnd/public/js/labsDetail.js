eadfe864958d49ae4c8cb69b331093d8112d349e
bh-lay <bh_lay@126.com>
2014-04-11 18:20:50 +0800

    forget

--- web/frontEnd/public/js/labsDetail.js
+++ web/frontEnd/public/js/labsDetail.js
@@ -11,4 +11,20 @@ seajs.use('/frontEnd/util/tie.js',function(){
 	$.getScript('/frontEnd/lib/juicer.js',function(){
 		$.getScript('/frontEnd/lib/github/reposWidge.js');
 	});
+
+
+
+
+	var btn = $('.labs_detail_download_link a');
+	btn.eq(0).addClass('active');
+
+	btn.mouseover(function(){//点击圆点按钮效果
+		var index = $(this).index();
+		$('.labs_detail_download_linkTxt ul:first').stop().animate({
+			'marginLeft' : '-' + index + '00%'
+		},200);
+		btn.removeClass('active');
+		btn.eq(index).addClass('active');
+	});
+	
 });
