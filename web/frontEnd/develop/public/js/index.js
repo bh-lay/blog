24402f2f7f51012f82f09827e8b0d8edc9543dc6
bh-lay <bh_lay@126.com>
2014-07-28 00:58:57 +0800

    change style

--- web/frontEnd/develop/public/js/index.js
+++ web/frontEnd/develop/public/js/index.js
@@ -7,20 +7,23 @@
 //index page
 define(function(require,exports){
 	
-	var temp = ['<div class="indeCnt"><div class="l_row"><div class="l_col_12">',
-		'<div class="layIntroCnt">',
-			'<div class="layIntro layIntroA"><span>我是</span><strong>剧中人</strong></div>',
-			'<div class="layIntro layIntroB">90后·天蝎男</div>',
-			'<div class="layIntro layIntroC">喜欢拆卸，喜欢代码，喜欢设计</div>',
-			'<div class="layIntro layIntroD">喜欢一切有创造力的事情</div>',
-			'<div class="laySnsLink">',
-				'<a target="_blank" class="sns_qq" title="QQ" href="http://sighttp.qq.com/authd?IDKEY=add2a4ef1d41f5d9014a7162fd89e6343c31c38ccbf9cc6f"><span>扣扣</span></a>',
-				'<a target="_blank" class="sns_weibo" title="新浪微博" href="http://weibo.com/bhlay"><span>微博</span></a>',
-				'<a target="_blank" class="sns_google" title="Google+" href="https://plus.google.com/117302426559268025818/"><span>谷歌+</span></a>',
-				'<a target="_blank" class="sns_zhanku" title="站酷" href="http://www.zcool.com.cn/u/684057/"><span>站酷</span></a>',
+	var temp = ['<div class="indeCnt">',
+		'<div class="index_intro">',
+			'<div class="index_intro_cpt">',
+				'<div class="index_intro_logo"><i class="l-icon">&#x0049;</i></div>',
+				'<h1 class="index_intro_a">小剧客栈</h1>',
+			'</div>',
+			'<div class="index_intro_cnt">',
+				'<p class="index_intro_b">一个自称采用了单页面加响应式的个人博客</p>',
+				'<p class="index_intro_c">nodeJS强力驱动</p>',
+				'<p class="index_intro_d">无耻地没有采用express框架</p>',
 			'</div>',
 		'</div>',
-		'<div class="webLink"><div class="webLinkBar">我的小伙伴</div>',
+		'<div class="some_link">',
+			'<a title="创业团" target="_blank" href="/topic/intiate.html">创业团</a>',
+			'<a title="给剧中人留言" target="_blank" href="/demo/bless.html">留言</a>',
+		'</div>',
+		'<div class="webLink"><div class="webLink_cpt">我的小伙伴</div>',
 		'<div class="webLink_cnt">',
 			'<a target="_blank" href="http://www.w3clover.com">W3C爱好者</a>',
 			'<a target="_blank" href="http://www.z990.com">小林Design</a>',
@@ -29,24 +32,10 @@ define(function(require,exports){
 			'<a target="_blank" href="http://www.woween.com">何东杰的网络日记</a>',
 			'<a target="_blank" href="http://www.w3cfuns.com/">前端开发网</a>',
 		'</div></div>',
-		'<div class="some_link">',
-			'<a title="创业团" target="_blank" href="/topic/intiate.html">创业团</a>',
-			'<a title="给剧中人留言" target="_blank" href="/demo/bless.html">留言</a>',
-		'</div>',
-	'</div></div></div>'].join('');
+	'</div>'].join('');
 	
 	return function(dom,callback){
 		var this_dom = $(temp);
 		dom.html(this_dom);
-		isShow = false;
-		dom.find('.webLinkBar').click(function(){
-			if(isShow){
-				isShow = false;
-				$(this).next().slideUp(80);
-			}else{
-				isShow = true;
-				$(this).next().slideDown(80);
-			}
-		});
 	};
 });
