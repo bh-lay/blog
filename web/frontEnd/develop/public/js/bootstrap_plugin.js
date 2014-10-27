4d836f5f0347b9e80bff62a26bf56994a574e520
bh-lay <bh_lay@126.com>
2014-10-27 23:56:17 +0800

    美化尝鲜版提示框，优化功能提示

--- web/frontEnd/develop/public/js/bootstrap_plugin.js
+++ web/frontEnd/develop/public/js/bootstrap_plugin.js
@@ -42,9 +42,22 @@ function isShowedToday(){
 		}
 	} 
 }
-var changeVersionTpl = ['<div style="text-align:center;padding: 20px 10px;">',
-	'<p>快试试</p>',
-	'<h3>尝鲜版</h3>',
+
+/**
+ * 进入尝鲜版
+ */
+function jumpToNewVersion(){
+		document.cookie='ui_version=js;path=/';
+		window.location.reload();
+}
+var changeVersionTpl = ['<div class="newVersionPop">',
+	'<div class="nVP_bj"><img src="http://layasset.qiniudn.com/images/version_switch.jpg" /></div>',
+	'<div class="nVP_txt">',
+		'<p class="nVP_1">你竟然还在使用<span>屌丝版</span></p>',
+		'<p class="nVP_2">小剧已为高级浏览器单独做了开发</p>',
+		'<p class="nVP_3"><a href="javascript:void(0)">使用尝鲜版</a></p>',
+		'<p class=nVP_4><a href="javascript:void(0)">搞什么鬼，还分版本？</a></p>',
+	'</div>',
 '</div>'].join('');
 /**
  * 显示版本提示框
@@ -59,22 +72,18 @@ function showTips(btn){
 	active_pop = UI.pop({
 		'html': changeVersionTpl,
 		'from': btn,
-		'width': 600,
+		'width': $('body').width() > 720 ? 700 : 300,
+		'height' : 430,
 		'closeFn' : function(){
 			active_pop = null;
 			var DATE = new Date();
 			var month = DATE.getMonth() + 1;
 			var date = DATE.getDate();
 			document.cookie = 'last_show_version_time=' + month + '-' + date + ';path=/;';
-		},
-		'confirm' : {
-			'btns' : ['大胆尝试','胆小不用'],
-			'callback' : function(){
-				document.cookie='ui_version=js;path=/';
-				window.location.reload();
-				return false;
-			}
 		}
+	});
+	$(active_pop.dom).on('click','.nVP_3 a',function(){
+		jumpToNewVersion();
 	});
 }
 function version_init(){
@@ -83,7 +92,11 @@ function version_init(){
 	//检测浏览器
 	if(isAdvancedBrowser()){
 		var $btn = $('<a href="javascript:void(0)" class="toNewVersion"><i class="glyphicon glyphicon-send"></i>尝鲜版</a>');
+		$btn.css('left','-200px');
 		$('body').append($btn);
+		$btn.animate({
+			'left':0
+		},200);
 		$btn.click(function(){
 			showTips($btn[0]);
 		});
@@ -97,6 +110,18 @@ function version_init(){
 }
 $.getScript(app_config.frontEnd_base + 'UI/dialog.js',function(){
 	version_init();
+	$('.nav_comment_link').click(function(){
+		UI.confirm({
+			'text' : '仅在“尝鲜版”下可用',
+			'from' : $(this)[0],
+			'mask' : false,
+			'btns' : ['进入尝鲜版','保持现状'],
+			'callback' : function(){
+				jumpToNewVersion();
+			}
+		});
+		return false;
+	});
 });
 
 
@@ -108,9 +133,14 @@ if($comments.length){
 		html = ['<div class="panel panel-default">',
 			'<div class="panel-heading">评论</div>',
 			'<div class="panel-body">',
-				'<h3>评论，请移步尝鲜版！</h3><a href="javascript:void(0)" type="button" class="btn btn-info">进入尝鲜版</a>',
+				'<h3>评论功能，仅在“尝鲜版”下可用！</h3><a href="javascript:void(0)" type="button" class="btn btn-info">进入尝鲜版</a>',
 			'</div>',
 		'</div>'].join('');
 	}
 	$comments.html(html);
+	
+	$comments.on('click','.btn',function(){
+		jumpToNewVersion();
+	});
+	
 }
