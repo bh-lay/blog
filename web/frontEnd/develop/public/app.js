95e884cb4ad0c2291e96d016d6f4367ea88106e0
bh-lay <bh_lay@126.com>
2014-11-28 11:33:05 +0800

    Revert "优化设备检测"
    This reverts commit 4078bc65b432482c81363c83c959c9a6b0ca6a03.

--- web/frontEnd/develop/public/app.js
+++ web/frontEnd/develop/public/app.js
@@ -7,48 +7,52 @@
  * 判断是否支持css属性
  * 兼容css3
  */
-var supports = (function () {
-    'use strict';
+var supports = (function() {
 	var styles = document.createElement('div').style,
-		vendors = ['Webkit', 'Khtml', 'Ms', 'O', 'Moz'];
+		vendors = 'Webkit Khtml Ms O Moz'.split(/\s/);
 	
-	return function (prop) {
-		var returns = false,
-            i = 0,
-            total = vendors.length;
-		if (styles.hasOwnProperty(prop)) {
+	return function(prop) {
+		var returns = false;
+		if ( prop in styles ){
 			returns = prop;
-		} else {
-			prop = prop.replace(/^[a-z]/, function (val) {
+		}else{
+			prop = prop.replace(/^[a-z]/, function(val) {
 				return val.toUpperCase();
 			});
-			for (i; i < total; i++) {
-				if (styles.hasOwnProperty(vendors[i] + prop)) {
+			for(var i=0,total=vendors.length;i<total;i++){
+				if ( vendors[i] + prop in styles ) {
 					returns = ('-' + vendors[i] + '-' + prop).toLowerCase();
 				}
 			}
 		}
 		return returns;
 	};
-}());
+})();
 /**
  * 是否支持 canvas
- * 是否支持touch
  */
-var supports_canvas = !!document.createElement('canvas').getContext ? true : false;
-var isSupportTouch = document.hasOwnProperty("ontouchend") ? true : false;
+function supports_canvas() {
+  return !!document.createElement('canvas').getContext;
+}
 /**
- * 是否为高级浏览器
- * 是否为手机浏览器
+ * 检测是否为高级浏览器
  *
  */
-var isAdvancedBrowser = (supports('transition') && supports('transform') && supports_canvas) ? true : false;
-var isMobileBrowser = ($(window).width() && isSupportTouch) < 720 ? true : false;
-
-
+function isAdvancedBrowser(){
+	if(supports('transition') && supports('transform') && supports_canvas()){
+		return true;
+	}else{
+		return false;
+	}
+}
+/**
+ * 检测是否为手机浏览器
+ *
+ */
+var isMobileBrowser = $(window).width() < 720 ? true : false;
 
 //屌丝就用屌丝版
-if (!isAdvancedBrowser) {
+if(!isAdvancedBrowser()){
 	document.cookie = 'ui_version=j1s;path=/;max-age=0';
 	window.location.reload();
 }
@@ -60,65 +64,88 @@ window.L = window.L || {};
  * 若url为绝对地址，则使用源图，且不处理剪裁缩放
  * L.qiniu(url,config);
  */
-(function (exports) {
-    'use strict';
-	function cover(url, config) {
-		var w = config.width || config.height,
-		    h = config.height || config.width;
+(function(exports){
+	function cover(url,config){
+		w = config.width || config.height;
+		h = config.height || config.width;
 		return url + '?imageView/1/w/' + w + '/h/' + h + '/q/85';
 	}
-	function zoom(url, config) {
+	function zoom(url,config){
 		var confStr;
-		if (config.width) {
-			confStr = 'w/' + config.width;
-		} else {
-			confStr = 'h/' + config.height;
+		if(config.width){
+			confStr = 'w/' + config.width
+		}else{
+			confStr = 'h/' + config.height
 		}
 		
 		return url + '?imageView2/2/' + confStr + '/q/85';
 	}
 	var baseDomain = app_config.imgDomain;
-	exports.qiniu = function (url, config) {
+	exports.qiniu = function(url,config){
 		var src = url;
-		if (typeof (url) === 'string' && url.length > 0 && url[0] === '/') {
-			src = baseDomain + url;
-			if (config) {
-				if (config.type === "zoom") {
-					src = zoom(src, config);
-				} else {
+		if(typeof(url) == 'string' && url.length > 0 && url[0] == '/'){
+			src = baseDomain + url
+			if(config){
+				if(config.type == "zoom"){
+					src = zoom(src,config);
+				}else{
 					//config.type == "cover"
-					src = cover(src, config);
+					src = cover(src,config);
 				}
 			}
 		}
 		return src;
-	};
-}(L));
+	}
+})(L);
 
-function routerHandle(lofox) {
-    'use strict';
-	var dom = $('.contlayer'),
-        container = $('.app_container'),
-        $active_page = null;
+/**
+ * 模版引擎生成器
+ * L.tplEngine(str)
+ */
+(function(exports){
+	exports.tplEngine = function (str){
+		if(!str){
+			return;
+		}
+		return new Function("obj",
+			"var p=[];" +
+			"with(obj){p.push('" +
+			str
+			.replace(/[\r\t\n]/g, " ")
+			.split("<%").join("\t")
+			.replace(/((^|%>)[^\t]*)'/g, "$1\r")
+			.replace(/\t=(.*?)%>/g, "',$1,'")
+			.split("\t").join("');")
+			.split("%>").join("p.push('")
+			.split("\r").join("\\'")
+		+ "');}return p.join('');");
+	};
+})(L);
+
+function routerHandle(lofox){
+	var dom = $('.contlayer');
+	
+	var $active_page = null;
 	//移除老的page dom
-	function removePageDom(callback) {
-		if ($active_page) {
+	function removePageDom(callback){
+		if($active_page){
             var $old = $active_page;
 			$active_page = null;
 			$old.addClass('fadeOutRight');
-			setTimeout(function () {
+			setTimeout(function(){
 				$old.remove();
                 $('html,body').scrollTop(0);
                 callback && callback();
-			}, 500);
-		} else {
+			},500);
+		}else{
             callback && callback();
         }
 	}
+	var container = $('.app_container');
 	//显示单页dom
-	function getNewPage() {
+	function getNewPage(){
 		var newDom = $('<div class="page"><div class="l-loading-panel"><span class="l-loading"></span><p>正在加载模块</p></div></div>');
-        removePageDom(function () {
+        removePageDom(function(){
             container.append(newDom);
             newDom.addClass('fadeInLeft');
         });
@@ -129,36 +156,36 @@ function routerHandle(lofox) {
 	/**
 	 * 首页
 	 */
-	lofox.set('/', function () {
+	lofox.set('/',function(){
 		this.title('小剧客栈_剧中人的个人空间 网页设计师博客 互动设计学习者');
 		L.nav.setCur('/');
 		var dom = getNewPage();
 		
-		seajs.use('public/js/index.js', function (indexPage) {
+		seajs.use('public/js/index.js',function(indexPage){
 			indexPage(dom);
 		});
 	});
 	/**
 	 * 博文列表
 	 */
-	lofox.set('/blog', function (param, pathnde, search) {
+	lofox.set('/blog',function(param,pathnde,search){
 		this.title('我的博客_小剧客栈');
 		L.nav.setCur('blog');
 		var dom = getNewPage();
 		
-		seajs.use('public/js/blogList.js', function (blogList) {
-			blogList(dom, search);
+		seajs.use('public/js/blogList.js',function(blogList){
+			blogList(dom,search);
 		});
 	});
 	/**
 	 * 博客详细页
 	 */
-	lofox.set('/blog/{id}', function (param) {
+	lofox.set('/blog/{id}',function(param){
 		this.title('我的博客_小剧客栈');
 		L.nav.setCur('blog');
 		var dom = getNewPage();
-		seajs.use('public/js/blogDetail.js', function (blogDetail) {
-			blogDetail(dom, param.id, function (title) {
+		seajs.use('public/js/blogDetail.js',function(blogDetail){
+			blogDetail(dom,param.id,function(title){
 				lofox.title(title);
 			});
 		});
@@ -166,52 +193,52 @@ function routerHandle(lofox) {
 	/**
 	 * 我的分享列表
 	 */
-	lofox.set('/share', function () {
+	lofox.set('/share',function(){
 		this.title('我的分享_小剧客栈');
 		L.nav.setCur('share');
 		var dom = getNewPage();
-		seajs.use('public/js/shareList.js', function (shareList) {
+		seajs.use('public/js/shareList.js',function(shareList){
 			shareList(dom);
 		});
 	});
 	/**
 	 * 我的分享详细
 	 */
-	lofox.set('/share/{id}', function (param) {
+	lofox.set('/share/{id}',function(param){
 		this.title('我的分享_小剧客栈');
 		L.nav.setCur('share');
 		var dom = getNewPage();
 
-		seajs.use('public/js/shareDetail.js', function (shareDetail) {
+		seajs.use('public/js/shareDetail.js',function(shareDetail){
 			shareDetail(dom,param.id);
 		});
 	});
 	
 	//作品列表页
-	lofox.set('/opus', function () {
+	lofox.set('/opus',function(){
 		this.title('作品_小剧客栈');
 		L.nav.setCur('opus');
 		var dom = getNewPage();
-		seajs.use('public/js/opusList.js', function (opusList) {
+		seajs.use('public/js/opusList.js',function(opusList){
 			opusList(dom);
 		});
 	});
 	//作品详情页
-	lofox.set('/opus/{id}', function (param) {
+	lofox.set('/opus/{id}',function(param){
 		this.title('作品_小剧客栈');
 		L.nav.setCur('opus');
 		var dom = getNewPage();
-		seajs.use('public/js/opusDetail.js', function (opusDetail) {
-			opusDetail(dom, param.id);
+		seajs.use('public/js/opusDetail.js',function(opusDetail){
+			opusDetail(dom,param.id);
 		});
 	});
 	//实验室列表页
-	lofox.set('/labs', function () {
+	lofox.set('/labs',function(){		
 		this.title('实验室_小剧客栈');
 		
 		L.nav.setCur('labs');
 		var dom = getNewPage();
-		seajs.use('public/js/labsList.js', function (labsList) {
+		seajs.use('public/js/labsList.js',function(labsList){
 			labsList(dom);
 		});
 	});
@@ -219,24 +246,24 @@ function routerHandle(lofox) {
 	/**
 	 * 留言板
 	 */
-	lofox.set('/bless', function () {
+	lofox.set('/bless',function(){
 		this.title('留言板_小剧客栈');
 		L.nav.setCur('/');
 		var dom = getNewPage();
-		seajs.use('comments/index.js', function (comments) {
+		seajs.use('comments/index.js',function(comments){
 			dom.html('<div class="l_row blessPage"><div class="l_col_12"></div></div>');
-			new comments.init(dom.find('.l_col_12')[0], 'define-1');
+			new comments.init(dom.find('.l_col_12')[0],'define-1');
 		});
 	});
 	
 	/**
 	 * 监听页面跳转
 	 */
-	//lofox.on('change', function (url) {});
+	//lofox.on('change',function(url){});
 	
-	$('body').on('click', 'a[lofox="true"]', function () {
+	$('body').on('click','a[lofox="true"]',function(){
 		var url = $(this).attr('href');
-		setTimeout(function () {
+		setTimeout(function(){
 			lofox.push(url);
 			lofox.refresh();
 		});
@@ -249,31 +276,29 @@ seajs.use([
 	'util/lofox_1_0.js',
 	'lib/juicer.js',
 	'UI/dialog.js'
-], function (user) {
-    'use strict';
-    
+],function(user){
     //绑定路由
     var lofox = new util.lofox();
     routerHandle(lofox);
 
     //显示背景图
-    if (supports('backgroundSize') && !isMobileBrowser) {
+    if (supports('backgroundSize') && !isMobileBrowser){
         L.gallery();
     }
     //开始导航
     L.nav();
-    setTimeout(function () {
-        $('.app_mask').fadeOut(500, function () {
-            $(this).remove();
-        });
-    },500);
+   setTimeout(function(){
+       $('.app_mask').fadeOut(500,function(){
+           $(this).remove();
+       });
+   },500);
 	
     L.user = user;
-	L.push = function (url) {
-		lofox.push(url);
+	L.push = function(url){
+		lofox.push(url)
 	};
-	L.refresh = function () {
-		lofox.refresh();
+	L.refresh = function(){
+		lofox.refresh()
 	};
 });
 
@@ -284,46 +309,45 @@ seajs.use([
  * L.nav()
  * 
  */
-(function (ex) {
-    'use strict';
-	var init = function () {
-        $('.nav_moreBtn').click(function () {
-            $('body').toggleClass('nav_slidedown');
+(function(ex){
+	var init=function(){
+        $('.nav_moreBtn').click(function(){
+             $('body').toggleClass('nav_slidedown');
 		});
-		$('.app_nav .nav a').click(function () {
+		$('.app_nav .nav a').click(function(){
 			$('body').removeClass('nav_slidedown');
 		});
-		if (!isSupportTouch) {
+		if(!isMobileBrowser){
             var active_plane;
             var delay;
-            $('.nav a').on('mouseenter', function () {
+            $('.nav a').on('mouseenter',function(){
                 active_plane && active_plane.close();
-                var offset = $(this).offset(),
-                    title  = $(this).attr('data-title');
+                var offset = $(this).offset();
+                var title  = $(this).attr('data-title');
                 clearTimeout(delay);
-                delay = setTimeout(function () {
+                delay = setTimeout(function(){
                     active_plane = UI.plane({
                         'top' : offset.top,
                         'left' : 50,
                         'html' : '<div class="nav_tips">' + title + '</div>',
                         'from' : 'right',
-                        'closeFn' : function () {
+                        'closeFn' : function(){
                             active_plane = null;
                         }
                     });
-                }, 200);
-            }).on('mouseleave', function () {
+                },200);
+            }).on('mouseleave',function(){
                 clearTimeout(delay);
                 active_plane && active_plane.close();
             });
         }
 		
-		$('.nav_mask').on('click', function () {
+		$('.nav_mask').on('click',function(){
 			$('body').removeClass('nav_slidedown');
 		});
 		var active_pop;
-		$('.nav_setting').click(function () {
-			if (active_pop) {
+		$('.nav_setting').click(function(){
+			if(active_pop){
 				return;
 			}
 			var offset = $(this).offset();
@@ -332,118 +356,148 @@ seajs.use([
 				'from': $(this)[0],
 				'width': 400,
 				'html': '<div class="setting_pop"><a class="backToOldVersion" href="javascript:void(0)">回到屌丝版</a></div>',
-				'closeFn': function () {
+				'closeFn': function(){
 					active_pop = null;
 				}
 			});
 			
-			$(active_pop.dom).on('click', '.backToOldVersion', function () {
+			$(active_pop.dom).on('click','.backToOldVersion',function(){
 				document.cookie = 'ui_version=j1s;path=/;max-age=0';
 				window.location.reload();
 			});
 		});
 	};
 
-	function setCur(page) {
-		if (page === '/') {
+	var setCur = function(page){
+		if(page == '/'){
 			page = 'index';
 		}
 		$('.app_nav .nav li').removeClass('cur');
-		$('.app_nav .nav li[page=' + page + ']').addClass('cur');
-	}
+		$('.app_nav .nav li[page='+page+']').addClass('cur');
+	};
 	ex.nav = init;
 	ex.nav.setCur = setCur;
-}(L));
+})(L);
+
+/**
+ *
+ * 
+ */
+(function(exports){
+	var tpl = ['<style>',
+		'.myCard{position:fixed;width:260px;height:300px;background:#fff;top:60px;left:20%;z-index:100;border-radius:4px;overflow:hidden;box-shadow:0px 0px 100px rgba(0,0,0,0.2);}',
+		'.myCard_header{height:80px;background:#48f;}',
+		'.myCard_info{position:absolute;top:35px;left:20px;width:100%;}',
+		'.myCard_cover{width:60px;height:60px;margin-right:10px;float:left;background:#333;}',
+		'.myCard_cpt{width:150px;float:left;text-shadow:1px 1px 8px #000;}',
+		'.myCard_cpt h3{line-height:20px;font-size:16px;color:#fff;font-weight:bold;}',
+		'.myCard_cpt p{line-height:20px;font-size:14px;color:#fff;}',
+	'</style><div class="myCard">',
+		'<div class="myCard_header"></div>',
+		'<div class="myCard_info">',
+			'<div class="myCard_cover">',
+			'</div>',
+			'<div class="myCard_cpt">',
+				'<h3>剧中人</h3>',
+				'<p>前端工程师</p>',
+			'</div>',
+		'</div>',
+		
+	'</div>'].join('');
+	var card = function(){
+		$('body').append(tpl);
+	};
+	exports.myCard = function(){
+		return new card();
+	};
+})(L);
+
 
 /**
  * page background
  * L.gallery() 
  */
-(function (ex) {
-    'use strict';
+(function(ex){
 	var config = {
 		'delay' : 50000,
 		'coverData' : [
-			{'src': app_config.frontEnd_base + 'public/images/gallery/bamboo.jpg', 'alt': '竹子'},
-			{'src': app_config.frontEnd_base + 'public/images/gallery/coast.jpg', 'alt': '江边'}
+			{'src': app_config.frontEnd_base + 'public/images/gallery/bamboo.jpg','alt':'竹子'},
+			{'src': app_config.frontEnd_base + 'public/images/gallery/coast.jpg','alt':'江边'}
 		]
 	};
 	
-	function loadImg(src, param) {
-		var parm = param || {},
-			loadFn = parm.loadFn || null,
-			sizeFn = parm.sizeFn || null,
-			errorFn = parm.errorFn || null;
+	function loadImg(src,parm){
+		var parm = parm||{},
+			loadFn = parm['loadFn'] || null,
+			sizeFn = parm['sizeFn'] || null,
+			errorFn = parm['errorFn'] || null;
 		
-		var img = new Image(),
-            timer;
-		if (errorFn) {
-			img.onerror = function () {
+		var img = new Image();
+		if(errorFn){
+			img.onerror = function(){
 				errorFn();
-			};
+			}
+		};
+		if(loadFn){
+			img.onload = function(){
+				loadFn(img.width,img.height);
+			}
 		}
-		if (loadFn) {
-			img.onload = function () {
-				loadFn(img.width, img.height);
-			};
-		}
-		if (sizeFn) {
-			timer = setInterval(function () {
-				if (img.width > 1) {
+		if(sizeFn){
+			var timer = setInterval(function(){
+				if(img.width>1){
 					clearInterval(timer);
-					sizeFn(img.width, img.height);
+					sizeFn(img.width,img.height);
 				}
-			}, 2);
+			},2);
 		}
-		img.src = src;
-	}
-	function CSS3(d, b) {
-		var data = d,
+		img.src=src;
+	};
+	function CSS3(data,bj){
+		var data = data,
 			isWebkit = false,
-			bj = b,
+			bj = bj,
 			total = data.length;
 		bj.html('');
+		show(0);
 		
-		if (supports('webkitAnimation')) {
+		if(supports('webkitAnimation')){
 			isWebkit = true;
 		}
 		
-		function show(i) {
-			var index = i,
+		function show(index){
+			var index = index,
 				src = data[index].src;
-			loadImg(src, {
-                'loadFn' : function () {
-                    var newPic = $('<div class="galBj_mask"></div>');
-                    newPic.css({'backgroundImage' : 'url(' + src + ')'});
-                    bj.html(newPic);
+			loadImg(src,{'loadFn':function(){
+				var newPic = $('<div class="galBj_mask"></div>');
+				newPic.css({'backgroundImage' : 'url(' + src + ')'});
+				bj.html(newPic);
+				
+				if(!isWebkit){
+					newPic.hide().fadeIn(1000);
+				}
 
-                    if (!isWebkit) {
-                        newPic.hide().fadeIn(1000);
-                    }
-
-                    setTimeout(function () {
-                        bj.css({'backgroundImage' : 'url(' + src + ')'});
-                        newPic.hide();
-                        index++;
-                        index === total && (index = 0);
-                        show(index);
-                    }, config.delay);
-                }
-            });
+				setTimeout(function(){
+					bj.css({'backgroundImage' : 'url(' + src + ')'});
+					newPic.hide()
+					index++;
+					index == total&&(index=0);
+					show(index);
+				},config.delay);
+			}});
 		}
-		show(0);
 	}
-	ex.gallery = function () {
-		var $gallery = $('<div class="gallayer"><div class="galBj"></div><div class="galMask"></div></div>').hide(),
+	ex.gallery = function(){
+		var $gallery = $('<div class="gallayer"><div class="galBj"></div><div class="galMask"></div></div>').hide();
             $bj = $gallery.find('.galBj'),
-			data = config.coverData;
+			data = config['coverData'];
         $('body').prepend($gallery);
         $gallery.fadeIn(200);
-        CSS3(data, $bj);
-	};
+        CSS3(data,$bj);
+	}
 }(L));
 
 try {
     console.log("\n\n\n\n\n%c", 'font-size:0;line-height:0;padding:69px 80px 0 0;background:url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAFAAAABFCAMAAADq1JG3AAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAABhQTFRFr6+vhoaG6urqWlpaJiYm0NDQ////AAAAwk8ZEQAAAhNJREFUeNrMmNuW5CAIRQ8C5f//8ZhLTY8JINHMWs1zahegwgF8YisKYpbdmAlaBj9ACKOGqp01LHQOqHRh/UCZ9CmwgD3cydQnwIarQ2OkgRncjtQUsFDNmlAZAyH1gd2dxNW9R7zmJEJg4frYLmF3QJ3gtbCLB1SpdZmIdV5PxFL+DCLe4LWTuQOpLhmuQKzxqmgPnD+QaxpPINdlo3+BWOd9g96BZVBMCVBFay7hd/wDpIiGki3k+AJ9B+XePIL3zl8gPSrJQcXEAXQdJKcHu4+KDyCSpXPcJbaDhnsHIwngZYk2oBMxxQqFvZjhRMwDDWO70WKG7b3oZySKnDzB/iv6DI3tmGHWGSljoPNDYNJB20VpcnIqg24WYQojzvDsgybzYaYitmNmE4gckEyglQjNAWFWgMlL81+A+uuB+ZCXDuX1a/P6xX796b1eHF4vX2ad5IUCa7cAzJ3x1gLs3jC+2+r0Iq+NjrLoNOatjTpqGBMBb4G5UkTwvCsfUmRCLHmK7hRLnpxzie6IziPBaQtEX8OegjMYUgTl7p6Eo8pQtPdeKmVEezxWyL6gaqaDueJ4r8hNjtvqKzc/IjfbJoCUGx5bwC3iw7Av6rwP++HRDHpbmt1P2c7k30sLr1xGaz0ldouJsyIQirvAdeijeIkxwp0PRqzLim4rJ/Fu0Eb23/cFoB3keMvaRX77/o8AAwCZhYbH8b+xCwAAAABJRU5ErkJggg==");');
     console.log("coding and coding\r\nyear after year\r\n\r\nfollow me https://github.com/bh-lay");
-} catch (e) {}
+} catch(e) {}
--- web/frontEnd/develop/public/js/labsList.js
+++ web/frontEnd/develop/public/js/labsList.js
@@ -4,18 +4,19 @@
  */
 define(function(require,exports){
 	
-	var temp = ['{@each list as it}<div class="lab_item">',
-		'<a class="lab_cover" href="/labs/${it.name}" target="_blank" title="${it.title}">',
-			'<img src="${it.cover}" />',
+	var temp = ['<div class="lab_item">',
+		'<a class="lab_cover" href="/labs/<%=name %>" target="_blank" title="<%=title%>">',
+			'<img src="<%=cover %>" />',
 		'</a>',
 		'<h4 class="lab_title">',
-			'<a href="/labs/${it.name}" target="_blank" title="${it.title}">${it.title}</a>',
+			'<a href="/labs/<%=name %>" target="_blank" title="<%=title %>"><%=title %></a>',
 		'</h4>',
 		'<div class="lab_info">',
-			'<p>${it.intro}</p>',
+			'<p><%=intro %></p>',
 		'</div>',
-	'</div>{@/each}'].join('');
+	'</div>'].join('');
 	
+	var render = L.tplEngine(temp);
 	var limit = 20,
 		 skip = 0,
 		 count = null,
@@ -51,9 +52,10 @@ define(function(require,exports){
 		skip = 0;
 		getData(function(list){
 			dom.html('<div class="labsList"></div>');
-			var this_html = juicer(temp,{
-                'list' : list
-            });
+			var this_html = '';
+			for(var i=0,total=list.length;i<total;i++){
+				this_html += render(list[i]);
+			}
             dom.find('.labsList').append(this_html);
 		});
 	};
