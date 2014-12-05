bb1d45ffcd41127a2c449d6eaf3b725ef46ceff4
bh-lay <bh_lay@126.com>
2014-12-05 17:33:19 +0800

    修改屌丝版首页components

--- sys/component/indexMain.js
+++ sys/component/indexMain.js
@@ -11,7 +11,14 @@ exports.produce = function(temp,data,callback){
 	var method = mongo.start();
 	method.open({'collection_name':'blog_friend'},function(err,collection){
 		collection.find({}, {limit:20}).toArray(function(err, docs) {
-			var html = utils.juicer(temp,{'friend_list':docs});
+			if(err){
+				callback && callback(err);
+				return
+			}
+			console.log(temp, docs);
+			var html = utils.juicer(temp,{
+				'list': docs
+			});
 			
 			callback && callback(null,html);
 			method.close();
