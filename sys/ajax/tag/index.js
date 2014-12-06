a74722948c9b5e075d8d2ec7f05484fd2965b909
bh-lay <bh_lay@126.com>
2014-12-06 22:21:26 +0800

    为标签接口增加缓冲机制

--- sys/ajax/tag/index.js
+++ sys/ajax/tag/index.js
@@ -4,75 +4,95 @@
  */
 
 var mongo = require('../../core/DB.js');
-var parse = require('../../core/parse.js');
 
+function getTagsList(callback){
+	var method = mongo.start();
+	method.open({'collection_name':'article'},function(err,collection){
+		collection.find().toArray(function(err, docs) {
+			method.close();
+			if(err){
+				callback && callback(err);
+				return;
+			}
+			
+			var tagsObj = {};
+			var tagsArray = [];
+			//获取所有标签
+			for(var i=0,total=docs.length;i<total;i++){
+				var this_tags = docs[i].tags;
+				if(Object.prototype.toString.call(this_tags) == '[object Array]'){
+					for(var s=0,count=this_tags.length;s<count;s++){
+						var tagStr = this_tags[s];
+						tagsObj[tagStr] = tagsObj[tagStr] ? tagsObj[tagStr] + 1 : 1;
+					}
+				}
+			}
+			//转换为数组
+			for(var i in tagsObj){
+				tagsArray.push({
+					name : i,
+					count : tagsObj[i]
+				});
+			}
+			//排序
+			tagsArray.sort(function(x,y){
+				if(x.count > y.count){
+					return -1;
+				}else{
+					return 1;
+				}
+			});
+			callback && callback(null,tagsArray);
+		});
+	});
+}
+
+function getAllBlogTagsList(callback){
+	var method = mongo.start();
+	method.open({'collection_name':'article'},function(err,collection){
+		collection.find().toArray(function(err, docs) {
+			method.close();
+			if(err){
+				callback && callback(err);
+				return;
+			}
+			var tags = [];
+			for(var i=0,total=docs.length;i<total;i++){
+				tags.push({
+					id: docs[i].id,
+					title: docs[i].title,
+					tag:docs[i].tags
+				});
+			}
+			callback && callback(null,tags);
+		});
+	});
+}
 
 //获取所有博文的标签
-exports.allBlogList = function (connect,app){
-	var method = mongo.start();
-	method.open({'collection_name':'article'},function(err,collection){
-      //count the all list
-		var resJSON = {};
-		collection.find().toArray(function(err, docs) {
-			method.close();
-			if(err){
-				resJSON.code = 2;
-			}else{
-				var tags = [];
-				for(var i=0,total=docs.length;i<total;i++){
-					tags.push({
-						id: docs[i].id,
-						title: docs[i].title,
-						tag:docs[i].tags
-					});
-				}
-				resJSON['list'] = tags;
-			}
-			connect.write('json',resJSON);
+exports.allBlogTagsList = function (connect,app){
+	app.cache.ajax('allBlogTagsList',function(this_cache){
+		connect.write('json',this_cache);
+	},function(save_cache){
+		getAllBlogTagsList(function(err,list){
+			save_cache(JSON.stringify({
+				code: err ? 500 : 200,
+				list : list
+			}));
 		});
 	});
 }
+
 
 exports.list = function (connect,app){
-	var method = mongo.start();
-	method.open({'collection_name':'article'},function(err,collection){
-      //count the all list
-		var resJSON = {};
-		collection.find().toArray(function(err, docs) {
-			method.close();
-			if(err){
-				resJSON.code = 2;
-			}else{
-				var tagsObj = {};
-				var tagsArray = [];
-				//获取所有标签
-				for(var i=0,total=docs.length;i<total;i++){
-					var this_tags = docs[i].tags;
-					if(Object.prototype.toString.call(this_tags) == '[object Array]'){
-						for(var s=0,count=this_tags.length;s<count;s++){
-							var tagStr = this_tags[s];
-							tagsObj[tagStr] = tagsObj[tagStr] ? tagsObj[tagStr] + 1 : 1;
-						}
-					}
-				}
-				//转换为数组
-				for(var i in tagsObj){
-					tagsArray.push({
-						name : i,
-						count : tagsObj[i]
-					});
-				}
-				tagsArray.sort(function(x,y){
-					if(x.count > y.count){
-                  		return -1;
-					}else{
-						return 1;
-					}
-				});
-				resJSON['list'] = tagsArray;
-			}
-			connect.write('json',resJSON);
+	app.cache.ajax('tagsListB',function(this_cache){
+		connect.write('json',this_cache);
+	},function(save_cache){
+		getTagsList(function(err,list){
+			save_cache(JSON.stringify({
+				code: err ? 500 : 200,
+				list : list
+			}));
 		});
 	});
-
-}
+};
