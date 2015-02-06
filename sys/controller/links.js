467538a32ee310529b7592738e48a2630a14d74b
bh-lay <bh_lay@126.com>
2015-02-06 14:22:25 +0800

    重新整理链接结构

--- sys/controller/links.js
+++ sys/controller/links.js
@@ -1,17 +1,40 @@
 //author bh-lay
+var utils = require('../core/utils/index.js');
+var mongo = require('../core/DB');
 
+function getList(callback){
+	var method = mongo.start();
+    method.open({'collection_name':'blog_friend'},function(err,collection){
+		collection.find({
+            isShow: '1'
+        }, {
+            limit:20
+        }).toArray(function(err, docs) {
+			if(err){
+				callback && callback(err);
+				return
+			}
+			callback && callback(null,docs);
+			method.close();
+		});
+	});
+};
 exports.render = function (connect,app){
 	//缓存机制
 	app.cache.use('links_page',['html'],function(this_cache){
 		connect.write('html',200,this_cache);
 	},function(save_cache){
-		//获取视图
-		app.views('links',{
-			'title' : '前端英雄榜_特大号的WEB屌丝们',
-			'keywords' : '剧中人,程序员,前端工程师,网页设计,设计师',
-			'description' : '一帮程序员,前端工程师,网页设计,设计师，一群纯屌丝！'
-		},function(err,html){
-			save_cache(html);
-		});
+        getList(function(err,list){
+            
+            //获取视图
+            app.views('links',{
+                title : '前端英雄榜_特大号的WEB屌丝们',
+                keywords : '剧中人,程序员,前端工程师,网页设计,设计师',
+                description : '一帮程序员,前端工程师,网页设计,设计师，一群纯屌丝！',
+                list: list
+            },function(err,html){
+                save_cache(html);
+            });
+        });
 	});
 }
