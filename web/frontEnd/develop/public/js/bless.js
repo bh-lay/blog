3d842f40f935cb530e2cc14c783a181457958c7e
bh-lay <bh_lay@126.com>
2015-02-28 22:59:53 +0800

    去除留言板页面的最新评论模块

--- web/frontEnd/develop/public/js/bless.js
+++ web/frontEnd/develop/public/js/bless.js
@@ -6,7 +6,7 @@ define(function(require,exports){
     var base_tpl = ['<div class="grid-row blessPage">',
         '<div class="grid-col-flow-300"></div>',
         '<div class="grid-col-fix-300 sidebar">',
-            '[-github_links-][-latest_comments-]',
+            '[-github_links-]',
         '</div>',
     '</div>'].join('');
 	
