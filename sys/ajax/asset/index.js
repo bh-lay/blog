a59e66e5e94aca3be48dca652fde5c391f2f0212
bh-lay <bh_lay@126.com>
2015-03-05 12:59:02 +0800

    图库功能统一配置权限

--- sys/ajax/asset/index.js
+++ sys/ajax/asset/index.js
@@ -8,16 +8,16 @@ var del = require('./delete');
 var rename = require('./rename');
 var createDir = require('./createDir');
 
-exports.render = function (connect,app){
-	var pathnode = connect.url.pathnode;
+function router(connect,app){
+    var pathnode = connect.url.pathnode;
 	if(pathnode.length == 2){
 		utils.parse.request(connect.request,function(err,data){
 			var pathStr = data.path;
 			
 			read.list(pathStr,function(err,files){
 				var json = {
-					'code' : 200,
-					'files' : files
+					code : 200,
+					files : files
 				}
 				if(err){
 					json.code = 404;
@@ -31,60 +31,78 @@ exports.render = function (connect,app){
 			upload.upload(connect.request,function(err,files){
 				if(err){
 					connect.write('json',{
-						'code' : 201
+						code : 201
 					});
 					return;
 				}
 				connect.write('json',{
-					'code' : 200,
-					'files' : files
+					code : 200,
+					files : files
 				});
 			});
 		}else if(pathnode[2] == 'del'){
 			del.file(connect.request,function(err){
 				if(err){
 					connect.write('json',{
-						'code' : 201
+						code : 201
 					});
 					return;
 				}
 				connect.write('json',{
-					'code' : 200
+					code : 200
 				});
 			});
 		}else if(pathnode[2] == 'delDir'){
 			connect.write('json',{
-				'code' : 203
+				code : 203
 			});
 		}else if(pathnode[2] == 'rename'){
 			rename.rename(connect.request,function(err){
 				if(err){
 					connect.write('json',{
-						'code' : 201
+						code : 201
 					});
 					return;
 				}
 				connect.write('json',{
-					'code' : 200
+					code : 200
 				});
 			});
 		}else if(pathnode[2] == 'createDir'){
 			createDir.createDir(connect.request,function(err){
 				if(err){
 					connect.write('json',{
-						'code' : 201
+						code : 201
 					});
 					return;
 				}
 				connect.write('json',{
-					'code' : 200
+					code : 200
 				});
 			});
-		}
+		}else{
+            connect.write('json',{
+			     code : 2,
+			     msg : 'wrong path'
+            });
+        }
 	}else{
 		connect.write('json',{
-			'code' : 2,
-			'msg' : 'wrong path'
+			code : 2,
+			msg : 'wrong path'
 		});
 	}
+}
+exports.render = function (connect,app){
+	connect.session(function(session_this){
+        if(session_this.get('user_group') == 'admin'){
+            router(connect,app);
+        }else{
+            connect.write('json',{
+                code : 201,
+                msg : 'no power'
+            });
+            
+        }
+    });
 }
