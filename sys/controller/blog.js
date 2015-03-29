8a88b1ea76aefb6c696c5e3aedb271902545a95f
bh-lay <bh_lay@126.com>
2015-03-29 23:48:17 +0800

    屌丝版作品列表增加分页

--- sys/controller/blog.js
+++ sys/controller/blog.js
@@ -18,95 +18,96 @@ function getDetail(id,callback){
       return;
     }
 
-		collection.find({
-			'id' : id
-		}).toArray(function(err, docs) {
-			method.close();
-			if(arguments[1].length==0){
-				callback && callback('哇塞，貌似这篇博文不存在哦!');
-			}else{
-				docs[0].time_show = utils.parse.time(docs[0].time_show ,'{y}-{m}-{d}');
-				
-				var converter = new showdown.converter();
-				docs[0].content = converter.makeHtml(docs[0].content);
-				callback&&callback(null,docs[0]);
-			}
-		});
-	});
+    collection.find({
+        'id' : id
+    }).toArray(function(err, docs) {
+        method.close();
+        if(arguments[1].length==0){
+            callback && callback('哇塞，貌似这篇博文不存在哦!');
+        }else{
+            docs[0].time_show = utils.parse.time(docs[0].time_show ,'{y}-{m}-{d}');
+
+            var converter = new showdown.converter();
+            docs[0].content = converter.makeHtml(docs[0].content);
+            callback&&callback(null,docs[0]);
+        }
+    });
+  });
 }
 
 function getList(app,param,callback){
-	var method = mongo.start();
-	var param = param || {};
-	var skip = param.skip || 0;
-	var limit = param.limit || 10;
-	
-	method.open({
+  var method = mongo.start();
+  var param = param || {};
+  var skip = param.skip || 0;
+  var limit = param.limit || 10;
+
+  method.open({
     collection_name: 'article'
   },function(err,collection){
     if(err){
       callback && callback(err);
       return;
     }
-		collection.count(function(err,count){
-			collection.find({}, {
-				limit:limit
-			}).sort({id:-1}).skip(skip).toArray(function(err, docs) {
-				method.close();
-				for(var i in docs){
-					docs[i].time_show = utils.parse.time(docs[i].time_show ,'{y}年-{m}月-{d}日');
-					docs[i].cover = (docs[i].cover && docs[i].cover[0] == '/') ? app.config.frontEnd.img_domain + docs[i].cover : docs[i].cover;
-				}
-				callback && callback(null,docs,{
-					'count' : count,
-					'skip' : skip,
-					'limit' : limit
-				});
-			});
-		});
-	});
+    collection.count(function(err,count){
+        collection.find({}, {
+            limit:limit
+        }).sort({
+          id:-1
+        }).skip(skip).toArray(function(err, docs) {
+            method.close();
+            for(var i in docs){
+                docs[i].time_show = utils.parse.time(docs[i].time_show ,'{y}年-{m}月-{d}日');
+                docs[i].cover = (docs[i].cover && docs[i].cover[0] == '/') ? app.config.frontEnd.img_domain + docs[i].cover : docs[i].cover;
+            }
+            callback && callback(null,docs,{
+              count : count,
+              skip : skip,
+              limit : limit
+            });
+        });
+    });
+  });
 };
 exports.list = function (connect,app){
 	
-	var data = connect.url.search;
-	
-	var page = data.page || 1;
-	
-	var cache_name = 'blog_list_' + page;
-	app.cache.use(cache_name,['html'],function(this_cache){
-		//do something with this_cache
-		connect.write('html',200,this_cache);
-	},function(save_cache){
-		//if none of cache,do this Fn
-		getList(app,{
-			'skip' : (page-1) * 10,
-			'limit': 10
-		},function(err,list,data){
+  var data = connect.url.search;
+  var page = data.page || 1;
+
+  var cache_name = 'blog_list_' + page;
+  app.cache.use(cache_name,['html'],function(this_cache){
+    //do something with this_cache
+    connect.write('html',200,this_cache);
+  },function(save_cache){
+    //if none of cache,do this Fn
+    getList(app,{
+      skip : (page-1) * 10,
+      limit: 10
+    },function(err,list,data){
       if(err){
         app.views('system/mongoFail',{},function(err,html){
           connect.write('html',500,html);
         })
         return;
       }
-			var page_html = app.utils.pagination({
-				list_count : data.count,
-				page_list_num: data.limit,
-				page_cur: page,
-				max_page_btn: 10,
-				base_url : '/blog?page={num}'
-			});
-			//获取视图
-			app.views('blogList',{
-				'title' : '我的博客',
-				'keywords' : '剧中人,bh-lay,网站建设,网页设计,设计师',
-				'description' : '小剧客栈是剧中人精心营造的一个向广大设计爱好者、喜欢剧中人开放的博客，小剧希望用设计师鞭策自己，愿意和你共同分享，一起进步！',
-				'list' : list,
-				'pagination' : page_html
-			},function(err,html){
-				save_cache(html);
-			});
-		});
-	});
+      var page_html = app.utils.pagination({
+          list_count : data.count,
+          page_list_num: data.limit,
+          page_cur: page,
+          max_page_btn: 10,
+          base_url : '/blog?page={num}'
+      });
+      //获取视图
+      app.views('blogList',{
+          'title' : '我的博客',
+          'keywords' : '剧中人,bh-lay,网站建设,网页设计,设计师',
+          'description' : '小剧客栈是剧中人精心营造的一个向广大设计爱好者、喜欢剧中人开放的博客，小剧希望用设计师鞭策自己，愿意和你共同分享，一起进步！',
+          'list' : list,
+          'pagination' : page_html
+      },function(err,html){
+          save_cache(html);
+      });
+    });
+  });
 };
 
 exports.detail = function (connect,app,id){
--- sys/controller/opus.js
+++ sys/controller/opus.js
@@ -5,9 +5,11 @@ var utils = require('../core/utils/index.js');
 var mongo = require('../core/DB.js');
 var showdown = require('../lib/showdown/showdown.js');
 
-function list_page(app,callback){
-	var method = mongo.start();
-
+function list_page(app,param,callback){
+  var method = mongo.start();
+  var param = param || {};
+  var skip = param.skip || 0;
+  var limit = param.limit || 10;
 	method.open({
     collection_name: 'opus'
   },function(err,collection){
@@ -15,16 +17,25 @@ function list_page(app,callback){
       callback && callback(err);
       return
     }
-		collection.find({}, {limit:28}).sort({id:-1}).toArray(function(err, docs) {
-			method.close();
-			for(var i = 0,total = docs.length;i<total;i++){
-				docs[i]['work_range'] = docs[i]['work_range']?docs[i]['work_range'].split(/\,/):['暂未填写'];
-				docs[i].cover = (docs[i].cover && docs[i].cover[0] == '/') ? app.config.frontEnd.img_domain + docs[i].cover : docs[i].cover;
-			}
-			callback && callback(null,docs);
-		});
-
-	});
+    collection.count(function(err,count){
+      collection.find({}, {
+        limit:limit
+      }).sort({
+        id:-1
+      }).skip(skip).toArray(function(err, docs) {
+        method.close();
+        for(var i = 0,total = docs.length;i<total;i++){
+          docs[i]['work_range'] = docs[i]['work_range']?docs[i]['work_range'].split(/\,/):['暂未填写'];
+          docs[i].cover = (docs[i].cover && docs[i].cover[0] == '/') ? app.config.frontEnd.img_domain + docs[i].cover : docs[i].cover;
+        }
+        callback && callback(null,docs,{
+          count : count,
+          skip : skip,
+          limit : limit
+        });
+      });
+    });
+  });
 }
 
 function detail_page(id,callback){
@@ -51,52 +62,67 @@ function detail_page(id,callback){
 }
 
 exports.list = function(connect,app){
-	app.cache.use('opus_list',['html'],function(this_cache){
-		connect.write('html',200,this_cache);
-	},function(save_cache){
-		list_page(app,function(err,list){
+  var data = connect.url.search;
+  var page = data.page || 1;
+  
+  app.cache.use('opus_list_' + page,['html'],function(this_cache){
+    connect.write('html',200,this_cache);
+  },function(save_cache){
+    list_page(app,{
+      skip: (page-1) * 10,
+      limit: 10
+    },function(err,list,data){
       if(err){
         app.views('system/mongoFail',{},function(err,html){
           connect.write('html',500,html);
         })
         return;
       }
-			//获取视图
-			app.views('opusList',{
-				'title' : '作品',
-				'keywords' : '剧中人,bh-lay,网站建设,网页设计,设计师',
-				'description' : '小剧客栈是剧中人精心营造的一个向广大设计爱好者、喜欢剧中人开放的博客，小剧希望用设计师鞭策自己，愿意和你共同分享，一起进步！',
-				'list' : list
-			},function(err,html){
-				save_cache(html);
-			});
-		});
-	});
+      
+      var page_html = app.utils.pagination({
+          list_count : data.count,
+          page_list_num: data.limit,
+          page_cur: page,
+          max_page_btn: 10,
+          base_url : '/opus?page={num}'
+      });
+      //获取视图
+      app.views('opusList',{
+          'title' : '作品',
+          'keywords' : '剧中人,bh-lay,网站建设,网页设计,设计师',
+          'description' : '小剧客栈是剧中人精心营造的一个向广大设计爱好者、喜欢剧中人开放的博客，小剧希望用设计师鞭策自己，愿意和你共同分享，一起进步！',
+          'list' : list,
+          'pagination' : page_html
+      },function(err,html){
+          save_cache(html);
+      });
+    });
+  });
 };
 
 exports.detail = function(connect,app,id){
-	app.cache.use('opus_id_' + id,['html'],function(this_cache){
-		connect.write('html',200,this_cache);
-	},function(save_cache){
-		detail_page(id,function(err,data){
+  app.cache.use('opus_id_' + id,['html'],function(this_cache){
+    connect.write('html',200,this_cache);
+  },function(save_cache){
+    detail_page(id,function(err,data){
       if(err){
         app.views('system/mongoFail',{},function(err,html){
           connect.write('html',500,html);
         })
         return;
       }
-			//获取视图
-			app.views('opusDetail',{
-				'title' : data.title,
-				'keywords' : data.tags,
-				'description' : data.intro,
-				'content' : data.content,
-				'opus_pic' : data.opus_pic,
-				'cover' : (data.cover && data.cover[0] == '/') ? app.config.frontEnd.img_domain + data.cover : data.cover,
-				'opus_time_create' : data.opus_time_create
-			},function(err,html){
-				save_cache(html);
-			});
-		});
-	});
+      //获取视图
+      app.views('opusDetail',{
+        title : data.title,
+        keywords : data.tags,
+        description : data.intro,
+        content : data.content,
+        opus_pic : data.opus_pic,
+        cover : (data.cover && data.cover[0] == '/') ? app.config.frontEnd.img_domain + data.cover : data.cover,
+        opus_time_create : data.opus_time_create
+      },function(err,html){
+          save_cache(html);
+      });
+    });
+  });
 }
