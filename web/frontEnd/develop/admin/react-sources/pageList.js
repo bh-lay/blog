e931b07ac0ccd5e065e8bbe2f07f2043f7b4e312
bh-lay <bh_lay@126.com>
2015-05-13 17:39:24 +0800

    尝试使用 react 实现后端部分逻辑

---
+++ web/frontEnd/develop/admin/react-sources/pageList.js
@@ -0,0 +1,39 @@
+define(function(){
+  var PageList = React.createClass({
+    jumpTo : function(e){
+      var page = parseInt(e.target.innerHTML);
+      this.props.onPageChange(page)
+    },
+    render: function() {
+      var list_count = this.props.count,
+          page_cur = this.props.page || 1,
+          page_list_num = this.props.limit || 10,
+          page_num = Math.ceil(list_count / page_list_num),
+          max_page_btn = 6;
+      
+      var pageNodes = [],
+          btn_num = 0,
+          start_num = page_num > max_page_btn ? page_cur - Math.floor(max_page_btn/2) : 0;
+      
+      pageNodes.push(page_cur > 1 ? <li><a data-page="prev" href="javascript:void(0)" >上一页</a></li> : <li className="disabled"><span>上一页</span></li>);
+
+        start_num = Math.max(start_num,1);
+        for(; start_num < page_num + 1; start_num++) {
+          if(start_num != page_cur){
+            pageNodes.push(<li><a href="javascript:void(0)" onClick={this.jumpTo}>{start_num}</a></li>);
+          }else{
+            pageNodes.push(<li className="active"><span>{start_num}</span></li>);
+          }
+          btn_num++;
+          if(btn_num >= max_page_btn){
+            break;
+          }
+        }
+      pageNodes.push(page_num - page_cur >= 1 ? <li><a data-page="next" href="javascript:void(0)">下一页</a></li> : <li className="disabled"><span>下一页</span></li>);
+      return (
+        <ul className="pagination">{pageNodes}</ul>
+      );
+    }
+  });
+  return PageList;
+});
