4e73af2b09fd585bceba810b3531d28464173bfa
bh-lay <bh_lay@126.com>
2015-06-17 21:07:33 +0800

    labs 错误地返回500改为404

--- sys/controller/labs.js
+++ sys/controller/labs.js
@@ -16,40 +16,40 @@ function list_page(app,callback){
       callback && callback(err);
       return
     }
-		collection.find({}, {limit:15}).sort({id:-1}).toArray(function(err, docs) {
-			for(var i in docs){
-				docs[i].cover = (docs[i].cover && docs[i].cover[0] == '/') ? app.config.frontEnd.img_domain + docs[i].cover : docs[i].cover;
-			}
-			method.close();
-			callback && callback(null,docs);
-		});
-	});
+    collection.find({}, {limit:15}).sort({id:-1}).toArray(function(err, docs) {
+      for(var i in docs){
+        docs[i].cover = (docs[i].cover && docs[i].cover[0] == '/') ? app.config.frontEnd.img_domain + docs[i].cover : docs[i].cover;
+      }
+      method.close();
+      callback && callback(null,docs);
+    });
+  });
 }
 
 function get_detail(lab_name,callback){
 	//get template
-	var method = mongo.start();
+  var method = mongo.start();
 
-	method.open({
+  method.open({
     collection_name: 'labs'
   },function(err,collection){
     if(err){
       callback && callback(err);
       return
     }
-		collection.find({'name':lab_name}).toArray(function(err, docs) {
-			method.close();
-			if(arguments[1].length==0){
-				callback&&callback('哇塞，貌似这个插件不存在哦!');
-			}else{
-				docs[0].time_show = utils.parse.time(docs[0].time_create ,'{y}-{m}-{d}');
-			//	docs[0].content = markdown.parse(docs[0].content);
-				docs[0].content = converter.makeHtml(docs[0].content);
-			//	callback&&callback(docs[0].content);
-				callback&&callback(null,docs[0]);
-			}
-		});
-	});
+    collection.find({'name':lab_name}).toArray(function(err, docs) {
+      method.close();
+      if(arguments[1].length==0){
+          callback&&callback('notFound');
+      }else{
+        docs[0].time_show = utils.parse.time(docs[0].time_create ,'{y}-{m}-{d}');
+    //	docs[0].content = markdown.parse(docs[0].content);
+        docs[0].content = converter.makeHtml(docs[0].content);
+    //	callback&&callback(docs[0].content);
+        callback&&callback(null,docs[0]);
+      }
+    });
+  });
 }
 
 
@@ -78,30 +78,34 @@ exports.list = function (connect,app){
 };
 
 exports.detail = function (connect,app,lab_name){
-	app.cache.use('labs_id_' + lab_name,['html'],function(this_cache){
-		connect.write('html',200,this_cache);
-	},function(save_cache){
-		//获取作品信息
-		get_detail(lab_name,function(err,data){
-			if(err){
-				connect.write('error','怎么坏掉了呢！');
-				return
-			}
-			//获取视图
-			app.views('labsDetail',{
-				title : data.title + '_小剧客栈_剧中人的个人博客',
-				keywords : data.tags,
-				description : data.intro,
-				content : data.content,
-				git_full_name : data.git_full_name,
-				demo_url : data.demo_url
-			},function(err,html){
-				if(err){
-					connect.write('error','怎么坏掉了呢！');
-					return
-				}
-				save_cache(html);
-			});
-		});
-	});
+  app.cache.use('labs_id_' + lab_name,['html'],function(this_cache){
+    connect.write('html',200,this_cache);
+  },function(save_cache){
+    //获取作品信息
+    get_detail(lab_name,function(err,data){
+      if(err){
+        if(err == 'notFound'){
+          connect.write('notFound','怎么坏掉了呢！');
+        }else{
+          connect.write('error','怎么坏掉了呢！');
+        }
+        return
+      }
+      //获取视图
+      app.views('labsDetail',{
+        title : data.title + '_小剧客栈_剧中人的个人博客',
+        keywords : data.tags,
+        description : data.intro,
+        content : data.content,
+        git_full_name : data.git_full_name,
+        demo_url : data.demo_url
+      },function(err,html){
+        if(err){
+          connect.write('error','怎么坏掉了呢！');
+          return
+        }
+        save_cache(html);
+      });
+    });
+  });
 };
