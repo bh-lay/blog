9da12d4b038b7ff169f4cd5d991bd7723b2f529b
bh-lay <bh_lay@126.com>
2015-07-15 11:48:26 +0800

    页面完成返回顶部交互

--- web/frontEnd/develop/public/js/navigation.js
+++ web/frontEnd/develop/public/js/navigation.js
@@ -14,17 +14,35 @@ define(function () {
         }).on('click','.nav_moreBtn',function () {
 			$('body').toggleClass('nav_slidedown');
 		});
-        $('.backToOldVersion').on('click', function () {
-            UI.confirm({
-                text : '确定要去当屌丝？',
-                callback : function(){
-                    document.cookie = 'ui_version=html;path=/;';
-                    window.location.reload();
-                }
-            });
-        });
+		$('.backToOldVersion').on('click', function () {
+			UI.confirm({
+				text : '确定要去当屌丝？',
+				callback : function(){
+					document.cookie = 'ui_version=html;path=/;';
+					window.location.reload();
+				}
+			});
+		});
+		
+		function checkBackTop(){
+			var $win = $(window),
+					method = $win.scrollTop() > $win.height()*0.6 ? 'slideDown' : 'slideUp';
+			$back_top[method](100);
+		}
+		var delay,
+				$back_top = $('.back-top');
+		checkBackTop();
+		$(window).on('scroll',function(){
+			clearTimeout(delay);
+			delay = setTimeout(checkBackTop,100);
+		});
+		$back_top.click(function(){
+			$('html,body').animate({
+				scrollTop : 0
+			},200);
+		});
 	};
-
+	
 	function setCur(page) {
 		if (page === '/') {
 			page = 'index';
