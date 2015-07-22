9441f3b133ab9921e4582a4d79ec9a67386a69d0
bh-lay <bh_lay@126.com>
2015-07-22 11:35:34 +0800

    调整用户认证相关代码

--- web/frontEnd/develop/public/js/user.js
+++ web/frontEnd/develop/public/js/user.js
@@ -10,43 +10,6 @@
 
 define(function(require,exports){
   var hex_md5 = require('public/js/md5'),
-  
-      loginPanel_tpl = ['<div class="l_loginPanel">',
-        '<div class="l_loginPanel_tabs">',
-          '<a href="#" data-type="github"><i class="layIcon">A</i>github</a>',
-          '<a href="#" data-type="sina">微博</a>',
-          '<a href="#" data-type="account">帐号</a>',
-          '<a href="#" data-type="define">游客</a>',
-        '</div>',
-        '<div class="l_loginPanel_cnts">',
-          '<div class="l_loginPanel_item">',
-            '<div class="l_loginPanel_github">',
-              '使用github帐号登录',
-            '</div>',
-          '</div>',
-          '<div class="l_loginPanel_item">',
-            '<div class="l_loginPanel_sina">',
-              '新浪微博登录尚未开发',
-            '</div>',
-          '</div>',
-          '<div class="l_loginPanel_item">',
-            '<div class="l_loginPanel_account">',
-              '<input name="email" type="text" placeholder="邮箱"/>',
-              '<input name="password" type="password" placeholder="密码"/>',
-              '<input type="button" value="登录" data-type="account" />',
-            '</div>',
-          '</div>',
-          '<div class="l_loginPanel_item">',
-            '<div class="l_loginPanel_define">',
-              '<input name="nickname" type="text" placeholder="昵称" />',
-              '<input name="website" type="text" placeholder="个人主页" />',
-              '<input type="button" value="确定"  data-type="define" />',
-            '</div>',
-          '</div>',
-        '</div>',
-        '<div class="l_loginPanel_arrow">◆</div>',
-      '</div>'].join(''),
-  
       //存储当前用户的信息
       userInfo = null,
       //存储程序需要用到的登录回调
@@ -54,6 +17,7 @@ define(function(require,exports){
   
   //相应登录的回调函数
   window.appLoginCallback = function(data){
+    console.log(123,data);
     for(var i=0,total=LoginCallbacks.length;i<total;i++){
       LoginCallbacks[i](data);
     }
@@ -73,89 +37,7 @@ define(function(require,exports){
       }
     });
   }
-  function LOGIN(param,callback){
-    param = param || {};
-        var me = this;
-    
-    var pop = UI.pop({
-      width : 400,
-      html : loginPanel_tpl,
-      mask : false,
-      closeFn : param.closeFn
-    });
-    var $popDom = $(pop.dom);
-    this.loginFn = callback;
-    this.$arrow = $popDom.find('.l_loginPanel_arrow');
-    this.$cnts_item = $popDom.find('.l_loginPanel_item');
-    
-    $popDom.on('click','.l_loginPanel_tabs a',function(){
-      var type = $(this).attr('data-type');
-      me.tab(type);
-    }).on('click','input[data-type="account"]',function(){
-      var email = $popDom.find('input[name="email"]').val();
-      var password = $popDom.find('input[name="password"]').val();
-      $.ajax({
-        url : '/ajax/user/login',
-        type : 'POST',
-        data : {
-          email : email,
-          password : password
-        },
-        success : function(data){
-          if(data && data.code == 200){
-            pop.close();
-          }
-          me.loginFn && me.loginFn(data);
-          
-        }
-      });
-    }).on('click','input[data-type="define"]',function(){
-      me.loginFn && me.loginFn({
-        code : 200,
-        user : {
-          nickname : $popDom.find('input[name="nickname"]').val(),
-          website : $popDom.find('input[name="website"]').val()
-        }
-      });
-      pop.close();
-    });
-    //登录
-    this.tab('define');
-  }
-  LOGIN.prototype.tab = function(type){
-    var index,
-      me = this;
-    switch(type){
-      case 'github':
-        index = 0;
-        LoginCallbacks.push(this.loginFn);
-        window.open('https://github.com/login/oauth/authorize?client_id=150e88277697b41e0702&redirect_uri=http://bh-lay.com/snsLogin/github/');
-      break;
-      case 'sina':
-        index = 1;
-      break;
-      case 'define':
-        index = 3;
-      break;
-      //account
-      default:
-        index = 2;
-    }
-    this.$arrow.css('top',index*40);
-    this.$cnts_item.not(this.$cnts_item.eq(index)).animate({
-      left : '-100%'
-    },100,function(){
-      me.$cnts_item.eq(index).css({
-        left : 0,
-        display : 'none'
-      }).fadeIn(200);
-    });
-  };
-    
-    
-  exports.login = function (param,callback){
-    return new LOGIN(param,callback);
-  };
+  
   exports.setLocalUser = function(data){
     var data_str = JSON.stringify({
       username : data.username,
