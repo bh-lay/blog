1a8e27202f3ba664f94c1d14af2bcb1c95d7e24a
chenrenlei <chenrenlei@baidu.com>
2015-09-22 07:32:21 +0800

    哈哈，发布系统

--- frontEnd/build_aboutblog.sh
+++
@@ -1,4 +0,0 @@
-
-cd aboutblog
-
-fis3 release -d ../../web/topic/aboutblog --file fis-conf.js
---
+++ frontEnd/deploy.sh
@@ -0,0 +1,31 @@
+
+echo 'Deploy System'
+echo '-----------------------'
+
+# 源码目录-发布目录
+deployFrom=$1;
+deployTo='';
+
+if [ ! -d $deployFrom ]; then 
+    echo -e "project not found !\n";  
+    exit 1;
+fi
+
+case $deployFrom in
+    'aboutblog')
+		deployTo='../../web/topic/aboutblog'
+    ;;
+    'intiate')
+		deployTo='../../web/topic/intiate'
+    ;;
+    *)  
+		echo -e "need config first,please edit deploy.sh !\n";
+    	exit 1; 
+    ;;
+esac
+
+echo '  from '$deployFrom
+echo '  to   '$deployTo
+
+cd $deployFrom
+fis3 release -d $deployTo --file fis-conf.js
