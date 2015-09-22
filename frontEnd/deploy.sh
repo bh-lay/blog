98db9d448ae841f9903078c173d2b5f2610f8bfd
chenrenlei <chenrenlei@baidu.com>
2015-09-22 19:41:52 +0800

    deploy实现细节

--- frontEnd/deploy.sh
+++ frontEnd/deploy.sh
@@ -6,8 +6,14 @@ echo '-----------------------'
 deployFrom=$1;
 deployTo='';
 
+#检查项目是否存在
 if [ ! -d $deployFrom ]; then 
     echo -e "project not found !\n";  
+    exit 1;
+fi
+
+if [ ${deployFrom:0:1} == '_' ]; then 
+    echo -e "["$deployFrom"] is not a project !\n";  
     exit 1;
 fi
 
