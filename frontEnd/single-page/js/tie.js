d3edc460b3c6d406a81e71b6cd92d1c56fe063ec
剧中人 <bh_lay@126.com>
2015-10-23 19:12:45 +0800

    使用新的tie组件，消除内存泄露的隐患

--- frontEnd/single-page/js/tie.js
+++ frontEnd/single-page/js/tie.js
@@ -1,7 +1,7 @@
 /**
  * @author bh-lay
  * @github https://github.com/bh-lay/tie.js
- * @modified 2015-10-14 20:08
+ * @modified 2015-10-23 18:31
  *
  * 处理既要相对于某个模块固定，又要在其可视时悬浮的页面元素
  * util.tie({
@@ -155,28 +155,43 @@
 		return value;
 	}
 
-	function fix_position(scrollTop){
-		var top,position;
+	function resetPosition(){
+		var me = this,
+			scopeClient = getClient(me.scopeDom),
+			scrollTop = doc_scrollTop(),
+			state_before = me.state,
+			top,position;
 
-		if(this.state == 'min'){
+		me.minScrollTop = getClient(me.ghostDom).top - me.fix_top;
+		me.maxScrollTop = scopeClient.top + scopeClient.height - getClient(me.dom).height - me.fix_top;
+
+		if(scrollTop <= me.minScrollTop){
+			me.state = 'min';
 			top = 0;
 			position =  'relative';
-		}else if(this.state == 'max'){
-			var scopeClient = getClient(this.scopeDom);
-			top = scopeClient.top + scopeClient.height - getClient(this.ghostDom).top - getClient(this.dom).height;
+		}else if(scrollTop >= me.maxScrollTop){
+			me.state = 'max';
+			top = scopeClient.top + scopeClient.height - getClient(me.ghostDom).top - getClient(me.dom).height;
 			position = 'relative';
 		}else{
-			top = this.fix_top;
+			me.state = 'mid';
+			top = me.fix_top;
 			position = 'fixed';
 		}
-		setCss(this.dom, {
+
+		setCss(me.dom, {
 			top: top,
 			position: position
 		});
+
+		if(state_before != me.state){
+			me.onPositionChange && me.onPositionChange.call(me,me.state);
+			me.updateGhostDom();
+		}
 	}
-	function INIT(param){
-		if( !(this instanceof INIT)){
-			return new INIT(param);
+	function Tie(param){
+		if( !(this instanceof Tie)){
+			return new Tie(param);
 		}
 		var me = this,
 			scroll_delay;
@@ -203,9 +218,7 @@
 		me.onPositionChange = param.onPositionChange || null;
 		
 		me.state = 'min';
-		me._scroll_listener = function(){
-			me.refresh();
-		}
+		me._scroll_listener = resetPosition.bind(this);
 		
 		me.refresh();
 		
@@ -214,35 +227,18 @@
 		}
 		bindHandler(me.scrollDom,'scroll',me._scroll_listener);
 	}
-	INIT.prototype = {
-		refresh: function (){
-			var scopeClient = getClient(this.scopeDom);
-
-			this.minScrollTop = getClient(this.ghostDom).top - this.fix_top;
-			this.maxScrollTop = scopeClient.top + scopeClient.height - getClient(this.dom).height - this.fix_top;
-			var scrollTop = doc_scrollTop(),
-				state_before = this.state;
-			if(scrollTop <= this.minScrollTop){
-				this.state = 'min';
-			}else{
-				this.state = scrollTop >= this.maxScrollTop ? 'max' : 'mid';
-			}
-			fix_position.call(this,scrollTop);
-			if(state_before != this.state){
-				this.onPositionChange && this.onPositionChange.call(this,this.state);
-				this.updateGhostDom();
-			}
-		},
+	Tie.prototype = {
+		refresh: resetPosition,
 		updateGhostDom: function(){
 			this.ghostDom.style.height = getClient(this.dom).height + 'px';
 		},
 		destroy: function(){
-			bindHandler(this.scrollDom,'scroll',this._scroll_listener);
+			removeHandler(this.scrollDom,'scroll',this._scroll_listener);
 			setCss(this.dom,{
 				position: 'relative',
 				top: getClient(this.dom).top - getClient(this.ghostDom).top
 			});
 		}
 	};
-	return INIT;
+	return Tie;
 });
