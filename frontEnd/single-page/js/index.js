39446b3fb25d7653cb1f2133ca5ff0a13749b6ef
剧中人 <bh_lay@126.com>
2015-10-25 11:28:34 +0800

    补全首页背景对非webkit的渐入支持

--- frontEnd/single-page/js/index.js
+++ frontEnd/single-page/js/index.js
@@ -6,22 +6,27 @@
 
 //index page
 define(function(require,exports){
-  function view(dom,callback){
-    var me = this;
-    this.dom = dom;
-    var temp = __inline('/tpl/index.html');
+  var temp = __inline('/tpl/index.html');
+
+  function view(dom){
+    var $gallery;
+    
     dom.html(temp);
-    this.$gallery = dom.find('.gallayer');
+    $gallery = dom.find('.gallayer');
+
     setTimeout(function(){
-    	me.$gallery.show();
+      $gallery.addClass('zoom-show');
     },600);
+
+    return {
+      destroy: function() {
+        $gallery.css({
+          position: 'absolute',
+          top: $(window).scrollTop() - $('.app_container').offset().top,
+          height: $(window).height()
+        });
+      }
+    };
   }
-  view.prototype.destroy = function() {
-  	this.$gallery.css({
-  		position: 'absolute',
-  		top: $(window).scrollTop() - $('.app_container').offset().top,
-  		height: $(window).height()
-  	});
-  };
   return view;
 });
