234a046db4c0cd392c3125c8d10c9277d2013815
剧中人 <bh_lay@126.com>
2016-01-16 19:22:04 +0800

    调整标签active显示逻辑

--- sys/component/multi-page/tag.js
+++ sys/component/multi-page/tag.js
@@ -3,20 +3,14 @@ var getTags = require('../../functions/getTags.js'),
     utils = require('../../core/utils/index.js');
 
 exports.produce = function(temp,data,callback){
-  var tag = data.tag;
   getTags.getTagsList(function(err,list){
     if(list.length > 100){
       list.length = 100;
     }
-    if(tag){
-      for(var i=0,total=list.length;i<total;i++){
-        if(tag == list[i].name){
-          list[i].active = true;
-        }
-      }
-    }
+
     var html = utils.juicer(temp,{
-      list: list
+      list: list,
+      activeTag: data.tag
     });
     callback && callback(null,html);
   });
