66776ae8e9e61ab6de05cc89b7fd6494cfb51b00
bh_lay <bh_lay@126.com>
2016-03-02 18:42:05 +0800

    调整复制文字逻辑

--- frontEnd/single-page/js/funny.js
+++ frontEnd/single-page/js/funny.js
@@ -9,11 +9,12 @@ define(function(){
       document.title = document.hidden ? '出BUG了，快看！':'小剧客栈，剧中人的个人博客！';
     });
 
-    //改变复制文字
+    //复制超过18个字，改变被复制文字
     document.body.addEventListener('copy', function (event) {
-      var clipboardData = event.clipboardData || window.clipboardData;
+      var clipboardData = event.clipboardData || window.clipboardData,
+          innerText = window.getSelection().toString();
       
-      if (!clipboardData || !window.getSelection().toString()) {
+      if (!clipboardData || !innerText || innerText.length < 18) {
         return;
       }
       event.preventDefault();
@@ -21,11 +22,12 @@ define(function(){
           '来自：小剧客栈',
           '链接：' + window.location.href,
           '',
-          window.getSelection().toString()];
+          innerText];
       clipboardData.setData('text/html', data.join('<br>'));
       clipboardData.setData('text/plain',data.join('\n'));
     });
-    //控制条
+    
+    //控制台
     try {
       console.log("一个人到底多无聊\r\n 才会把 console 当成玩具\r\n一个人究竟多堕落\r\n 才会把大好青春荒废在博客上\r\n\r\n\r\n%cfollow me %c https://github.com/bh-lay","color:red","color:green");
     } catch (e) {}
