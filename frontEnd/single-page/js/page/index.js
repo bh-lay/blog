d56035f32022cb5a2c667b50ac6bf577b269c43b
bh_lay <bh_lay@126.com>
2016-08-30 20:42:40 +0800

    增加婚礼邀请函入口

--- frontEnd/single-page/js/page/index.js
+++ frontEnd/single-page/js/page/index.js
@@ -27,8 +27,33 @@ define([
       index = -1;
     }
     setTimeout(function(){
-      utils.addClass(nodeGallery,'zoom-show');
+      // utils.addClass(nodeGallery,'zoom-show');
+      nodeGallery.style.display = 'block';
     },600);
+
+// wedding
+    function leftZeroPad(val, minLength) {
+      if (typeof(val) != "string")
+        val = String(val);
+      return (MANY_ZEROS.substring(0, minLength - val.length)) + val;
+    }
+    function createCountDownHtml( str ){
+      var arr = str.match(/./g),
+        html = '';
+      arr.forEach(function( num ){
+        html += '<span>' + num + '</span>';
+      });
+      return html + '<strong>天</strong>';
+    }
+    var countDownDays = Math.ceil( ( new Date(2016,9-1,28) - new Date() )/1000/60/60/24 ),
+        MANY_ZEROS = "000000000000000000",
+        str_day = leftZeroPad(countDownDays,3),
+        html = createCountDownHtml( str_day );
+    utils.query('.countdown-body',node).innerHTML = html;
+    utils.css( utils.query('.index-wedding',node), {
+      height: window.innerHeight - 50
+    });
+// wedding
 
     return {
       destroy: function() {
