cbcf0fb3a802518bd20975c41f39b8b2fbc00f0e
bh_lay <bh_lay@126.com>
2017-03-18 13:00:39 +0800

    调整首页首屏显示

--- frontEnd/single-page/js/page/index.js
+++ frontEnd/single-page/js/page/index.js
@@ -8,12 +8,7 @@
 define([
   'js/Base'
 ],function(utils){
-  var temp = __inline('/tpl/index.html'),
-      imgs = [
-        __uri("/images/gallery/bamboo.jpg"),
-        __uri("/images/gallery/coast.jpg")
-      ],
-      index=-1;
+  var temp = __inline('/tpl/index.html');
 
   function view(global){
     var node = global.node,
@@ -21,24 +16,13 @@ define([
     node.innerHTML = temp;
 
     nodeGallery = utils.query('.gallayer',node);
-    nodeGallery.style.backgroundImage = 'url(' +  imgs[++index] + ')';
 
-    if(index + 1 >= imgs.length){
-      index = -1;
-    }
     setTimeout(function(){
       utils.addClass(nodeGallery,'zoom-show');
     },600);
 
-
-
     return {
       destroy: function() {
-        utils.css(nodeGallery,{
-          position: 'absolute',
-          top: utils.query('body').scrollTop - utils.offset(utils.query('.app_container')).top,
-          height: window.innerHeight
-        });
       }
     };
   }
