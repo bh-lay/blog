342f520b47bcb5e1000460db82e4f559f43ea810
剧中人 <bh_lay@126.com>
2017-09-23 00:11:27 +0800

    重新设计导航／首页

--- frontEnd/single-page/src/pages/index/index.js
+++ frontEnd/single-page/src/pages/index/index.js
@@ -7,14 +7,30 @@ import './index.less';
 import utils from '../../js/Base.js';
 
 const temp = require('./index.html');
-
+let potoGraphaList = [
+  {
+    title: '束河古城',
+    imgSrc: require('./images/aboutme_2.jpg'),
+    htmlSrc: 'https://bh-lay.tuchong.com/14591502/',
+  },
+  {
+    title: '西沙湿地',
+    imgSrc: require('./images/aboutme.jpg'),
+    htmlSrc: 'https://bh-lay.tuchong.com/14465332/',
+  }
+];
+let potoGraphaIndex = -1;
 function view (global) {
   const node = global.node;
   let nodeGallery;
   node.innerHTML = temp;
 
-  nodeGallery = utils.query('.gallayer', node);
+  nodeGallery = utils.query('.index-aboutme', node);
+  nodeGallery.style.backgroundImage = `url(${potoGraphaList[++potoGraphaIndex].imgSrc})`;
 
+  if (potoGraphaIndex + 1 >= potoGraphaList.length) {
+    potoGraphaIndex = -1;
+  }
   setTimeout(function () {
     utils.addClass(nodeGallery, 'zoom-show');
   }, 600);
