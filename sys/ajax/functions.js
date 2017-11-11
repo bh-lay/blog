a6cb5dc7c0a09a003a0d9b87bb703d91fa8ab5f8
剧中人 <bh_lay@126.com>
2017-11-11 23:25:59 +0800

    增加 functions API

--- sys/ajax/functions.js
+++ sys/ajax/functions.js
@@ -2,44 +2,52 @@
  * @author bh-lay
  */
 var utils = require('../core/utils/index.js'),
-    updateFriendsScore = require('../functions/updateFriendsScore.js');
+  updateFriendsScore = require('../functions/updateFriendsScore.js'),
+    my720Data = require('../functions/my720Data.js'),
+    myTuchongData = require('../functions/myTuchongData.js');
 
-function isAdmin( connect, successFn, failFn ){
-	connect.session(function(session_this){
-		if( session_this.get('user_group') == 'admin' ){
-			successFn && successFn();
-		}else{
-			failFn && failFn();
-		}
-	});
+function isAdmin (connect, successFn, failFn) {
+  connect.session(function (session_this) {
+    if (session_this.get('user_group') == 'admin') {
+      successFn && successFn();
+    } else {
+      failFn && failFn();
+    }
+  });
 }
 
-module.exports = function ( connect, app, act ){
-	if(connect.request.method != 'POST'){
-		connect.write('json',{
-			'code' : 201,
-			'msg' : 'please use POST to clear cache !'
-		});
-		return;
-	}
-	isAdmin( connect, function(){	
-		if( act == 'updateFriendsScore' ){
-			updateFriendsScore.update(function(){
-				connect.write('json',{
-					'code' : 200,
-					'msg' : 'update success !'
-				});
-			});
-		}else{
-			connect.write('json',{
-				'code' : 203,
-				'msg' : 'wrong action !'
-			});
-		}
-	},function(){
-		connect.write('json',{
-			'code' : 201,
-			'msg' : 'no power !'
-		});
-	});
+module.exports = function (connect, app, act) {
+  if (connect.request.method !== 'POST') {
+    connect.write('json', {
+      'code': 201,
+      'msg': 'please use POST to clear cache !'
+    });
+    return;
+  }
+  isAdmin(connect, function () {
+    var responseJson = {
+      code: 200,
+      msg: 'update success !'
+    };
+    switch (act) {
+      case 'updateFriendsScore':
+        updateFriendsScore.update();
+        break;
+      case 'update720yun':
+        my720Data.update();
+        break;
+      case 'updateTuchong':
+        myTuchongData.update();
+        break;
+      default:
+        responseJson.code = 203;
+        responseJson.msg = 'wrong action !';
+    }
+    connect.write('json', responseJson);
+  }, function () {
+    connect.write('json', {
+      'code': 201,
+      'msg': 'no power !'
+    });
+  });
 }
