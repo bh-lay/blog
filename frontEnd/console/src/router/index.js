f4914c58478e4d5052aa72a7bf79d5260d2de55a
剧中人 <bh_lay@126.com>
2017-12-27 18:12:25 +0800

    增加博文页面

--- frontEnd/console/src/router/index.js
+++ frontEnd/console/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '@/pages/login/index'
 import Panel from '@/pages/panel/index'
+import Article from '@/pages/content/article'
 
 Vue.use(Router)
 let routes = [{
@@ -17,6 +18,11 @@ let routes = [{
   path: '/panel',
   name: '控制台',
   component: Panel
+},
+{
+  path: '/content-article',
+  name: '博文页面',
+  component: Article
 }
 ]
 export default new Router({
