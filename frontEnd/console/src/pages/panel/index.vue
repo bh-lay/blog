5f825cbd3c115e4ef22ed410ab81a2c3cf12e7cd
剧中人 <bh_lay@126.com>
2017-12-29 01:15:32 +0800

    调整默认页面

--- frontEnd/console/src/pages/panel/index.vue
+++ frontEnd/console/src/pages/panel/index.vue
@@ -2,13 +2,24 @@
   <el-row :gutter="20">
     <el-col :span="16">
       <el-card class="box-card">
-        <h1>${username}你还有脸回来！</h1>
+        <h1>剧中人你还有脸回来！</h1>
         <p>你有多久没有更新博文了？</p>
         <p>博客还想不想要了？</p>
         <p>人生就此荒废你开心么？</p>
-        <p>
-          <a class="btn btn-primary btn-lg" href="/" target="_blank" role="button">去看看博客</a>
-        </p>
+        <a href="/" target="_blank">
+          <el-button type="primary">去看看博客</el-button>
+        </a>
+      </el-card>
+      <br>
+      <el-card class="box-card">
+        <h3>发布台
+          <small>增加博客内容</small>
+        </h3>
+        <div class="list-group">
+          <el-button>写博文</el-button>
+          <el-button>实验室</el-button>
+          <el-button>加友链</el-button>
+        </div>
       </el-card>
       <br>
       <el-card class="box-card">
@@ -26,25 +37,17 @@
       <el-card class="box-card">
         <h3>更新数据</h3>
         <p>默认每天更新一次，等不及的话可以手动更新。</p>
-        <a class="btn btn-default" title="更新前端英雄榜分数" href="/ajax/functions/updateFriendsScore" data-action-ajaxConfirm="确定要更新么？">更新前端英雄榜分数</a>
-        <a class="btn btn-default" title="更新720yun数据" href="/ajax/functions/update720yun" data-action-ajaxConfirm="确定要更新么？">更新720yun数据</a>
+
+        <el-button @click="executeFunction('updateFriendsScore')" >更新前端英雄榜分数</el-button>
+        <el-button @click="executeFunction('update720yun')">更新720yun数据</el-button>
       </el-card>
       <br>
       <el-card class="box-card">
         <h3>评论管理</h3>
         <p>好激动呦，有没有人给我写评论呢！</p>
-        <a class="btn btn-default custom-lofox" href="/admin/comments">评论管理</a>
-      </el-card>
-      <br>
-      <el-card class="box-card">
-        <h3>发布台
-          <small>增加博客内容</small>
-        </h3>
-        <div class="list-group">
-          <a class="list-group-item custom-lofox" href="/admin/publish/article">写博文</a>
-          <a class="list-group-item custom-lofox" href="/admin/publish/labs">实验室</a>
-          <a class="list-group-item custom-lofox" href="/admin/publish/friends">加友链</a>
-        </div>
+        <router-link to="/content-comments">
+          <el-button>评论管理</el-button>
+        </router-link>
       </el-card>
       <br>
       <el-card class="box-card">
@@ -52,7 +55,9 @@
           <small>静态资源管理</small>
         </h3>
         <p>远程镜像至七牛云存储，新增操作直接上传即可。修改或删除操作需要进入七牛后台进行重复操作。</p>
-        <a class="btn btn-info custom-lofox" href="/admin/gallery">进入图库</a>
+        <router-link to="/gallery">
+          <el-button>进入图库</el-button>
+        </router-link>
       </el-card>
     </el-col>
   </el-row>
@@ -68,10 +73,30 @@ export default {
   },
   methods: {
     clearCache () {
-      return fetch('/ajax/clear_cache?type=?' + this.selectedCaches.join(','), {
+      fetch('/ajax/clear_cache?type=?' + this.selectedCaches.join(','), {
         method: 'POST',
         credentials: 'same-origin'
-      }).then(response => response.json())
+      }).then(() => {
+        this.$message({
+          type: 'success',
+          message: '已发起清除缓存请求!'
+        })
+      })
+    },
+    executeFunction (functionName, confirmText) {
+      this.$confirm('确定要执行么？', '提示', {
+        type: 'warning'
+      }).then(() => {
+        fetch('/ajax/functions/' + functionName, {
+          method: 'POST',
+          credentials: 'same-origin'
+        }).then(() => {
+          this.$message({
+            type: 'success',
+            message: '已发起清除缓存请求!'
+          })
+        })
+      })
     }
   }
 }
