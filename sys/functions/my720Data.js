f764d565b0dda63c0d73acf6a91eb76c8ddf6319
剧中人 <bh_lay@126.com>
2019-03-03 00:10:54 +0800

    修复 720 云数据抓取

--- sys/functions/my720Data.js
+++ sys/functions/my720Data.js
@@ -2,7 +2,7 @@ var DB = require('../core/DB.js'),
 	collectionName = 'cache',
 	mongon_ID = '720yun_bh-lay',
 	request = require('request'),
-	clientUserAgent = 'Spider for http://bh-lay.com Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1';
+	clientUserAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.109 Safari/537.36';
 
 
 //从数据库读取
@@ -32,12 +32,11 @@ function saveDataToDataBase(data) {
 	data.id = mongon_ID;
 	DB.getCollection(collectionName)
 		.then(({ collection, closeDBConnect }) => {
-			//查询用户信息
-			collection.find({
-				id: mongon_ID
-			})
-				//计算条数
-				.countDocuments(function (err, count) {
+			// 计算条数
+			collection
+				.countDocuments({
+					id: mongon_ID
+				}, function (err, count) {
 					if (count > 0) {
 						// 条数存在，则直接更新
 						collection.updateOne({
@@ -65,15 +64,15 @@ function updateFrom720(callback) {
 		headers: {
 			'User-Agent': clientUserAgent,
 			'Accept': 'application/json, text/plain, */*',
-			'Accept-Language': 'zh-CN,zh;q=0.8,en;q=0.6,en-US;q=0.4',
-			// 'Access-Control-Request-Headers': 'app-authorization,app-key',
-			// 'Access-Control-Request-Method': 'GET',
-			// 'Cache-Control': 'no-cache',
-			// 'Connection': 'keep-alive',
-			// 'Host': 'apiv4.720yun.com',
-			// 'Origin': 'http://720yun.com',
-			// 'Pragma': 'no-cache',
-			'Referer': 'http://720yun.com/u/19023widcyv',
+			'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,en-US;q=0.7',
+			'Access-Control-Request-Headers': 'app-authorization,app-key',
+			'Access-Control-Request-Method': 'GET',
+			'Cache-Control': 'no-cache',
+			'Connection': 'keep-alive',
+			'Host': 'apiv4.720yun.com',
+			'Origin': 'https://720yun.com',
+			'Pragma': 'no-cache',
+			'Referer': 'https://720yun.com/u/19023widcyv',
 			'App-Authorization': '',
 			'App-Key': 'eByjUyLDG2KtkdhuTsw2pY46Q3ceBPdT'
 		}
@@ -87,10 +86,8 @@ function updateFrom720(callback) {
 			var userData = JSON.parse(body || {});
 			callback && callback(null, userData);
 			//保存到数据库
-			console.log('userData', userData)
 			saveDataToDataBase(userData);
 		} catch (e) {
-			console.warn('720yun data fetch failed ')
 			callback && callback('parse error', null);
 		}
 	});
