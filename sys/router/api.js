de67d1f4857217b6fd72d7f6e4dc729bff4d9a8b
剧中人 <bh_lay@126.com>
2019-03-05 20:49:33 +0800

    批量调整 API 目录结构

--- sys/router/api.js
+++ sys/router/api.js
@@ -5,81 +5,81 @@ let snsLogin = require('../controller/snsLogin.js')
  * ajax
  *
  */
-var ajax_user = require('../ajax/user/index')
-let ajax_add_edit = require('../ajax/add&edit.js')
-let ajax_comments = require('../ajax/comments/index.js')
-let ajax_links = require('../ajax/links/index.js')
-let ajax_del = require('../ajax/del')
-let ajax_demo = require('../ajax/demo/index')
-let ajax_tag =  require('../ajax/tag/index.js')
-let ajax_functions =  require('../ajax/functions.js')
+var user = require('../controller/api/user/index')
+let add_edit = require('../controller/api/add&edit.js')
+let comments = require('../controller/api/comments/index.js')
+let links = require('../controller/api/links/index.js')
+let del = require('../controller/api/del')
+let demo = require('../controller/api/demo/index')
+let tag =  require('../controller/api/tag/index.js')
+let functions =  require('../controller/api/functions.js')
 
 
 //实验室
-let ajax_labs = require('../ajax/labs_get')
-let ajax_labs_update = require('../ajax/labs/updateGitInfo')
+let labs = require('../controller/api/labs_get')
+let labs_update = require('../controller/api/labs/updateGitInfo')
 //清除缓存
-let ajax_clear_cache = require('../ajax/clear_cache')
+let clear_cache = require('../controller/api/clear_cache')
 // 获取全景图数据
-let ajax_pano = require('../ajax/pano_get.js')
+let pano = require('../controller/api/pano_get.js')
 // 获取图虫数据
-let ajax_photography = require('../ajax/photography_get.js')
+let photography = require('../controller/api/photography_get.js')
 //博客
-let ajax_blog = require('../ajax/article_get')
+let blog = require('../controller/api/article_get')
 
 
 module.exports = [
 	//通用增加&编辑
 	{
 		path: 'all /ajax/add_edit',
-		controller: ajax_add_edit.render
+		controller: add_edit.render
 	},
 	{
 		path: 'all /ajax/blog',
-		controller: ajax_blog.render
+		controller: blog.render
 	},
 	{
 		path: 'all /ajax/labs',
-		controller:ajax_labs.render
+		controller:labs.render
 	},
 	{
 		path: 'all /ajax/labs/updateGitInfo',
-		controller: ajax_labs_update.render
+		controller: labs_update.render
 	},
 	//友情链接
 	{
 		path: 'all /ajax/links/list',
-		controller: ajax_links.list
+		controller: links.list
 	},
 	{
 		path: 'all /ajax/links/detail/:id',
-		controller: ajax_links.detail
+		controller: links.detail
 	},
 	{
 		path: 'all /ajax/links/add_edit',
-		controller: ajax_links.add_edit
+		controller: links.add_edit
 	},
 	{
 		path: 'all /ajax/links/post',
-		controller: ajax_links.post
+		controller: links.post
 	},
 	// 清除缓存
 	{
 		path: 'all /ajax/clear_cache',
-		controller: ajax_clear_cache.render
+		controller: clear_cache.render
 	},
 	// 前端演示用的demo
 	{
 		path: 'all /ajax/demo/*',
 		controller(route, connect, app) {
-			ajax_demo.render(connect,app)
+			demo.render(connect,app)
 		}
 	},
 	// 公用删除接口
 	{
 		path: 'all /ajax/del',
 		controller(route, connect, app) {
-			ajax_del.render(connect,app)
+			del.render(connect,app)
 		}
 	},
 	// 评论
@@ -88,8 +88,8 @@ module.exports = [
 		controller(route, connect, app) {
 			var mark = route.param.mark
 			//尝试使用ajax模块提供接口
-			if(ajax_comments[mark]){
-				ajax_comments[mark](connect,app)
+			if(comments[mark]){
+				comments[mark](connect,app)
 			}else{
 				connect.write('json',{
 					'code' : 404
@@ -103,8 +103,8 @@ module.exports = [
 		controller(route, connect, app) {
 			var act = route.param.act
 			//尝试使用ajax模块提供接口
-			if(ajax_tag[act]){
-				ajax_tag[act](connect,app)
+			if(tag[act]){
+				tag[act](connect,app)
 			}else{
 				connect.write('json',{
 					'code' : 500
@@ -117,8 +117,8 @@ module.exports = [
 		controller(route, connect, app) {
 			var act = route.param.act
 			//尝试使用ajax模块提供接口
-			if(ajax_user[act]){
-				ajax_user[act](connect,app)
+			if(user[act]){
+				user[act](connect,app)
 			}else{
 				connect.write('json',{
 					'code' : 500
@@ -130,19 +130,19 @@ module.exports = [
 		path: 'all /ajax/functions/:act',
 		controller(route, connect, app) {
 			var act = route.param.act
-			ajax_functions(connect,app,act)
+			functions(connect,app,act)
 		}
 	},
 	{
 		path: 'all /ajax/pano/list',
 		controller(route, connect, app) {
-			ajax_pano.render( connect, app )
+			pano.render( connect, app )
 		}
 	},
 	{
 		path: 'all /ajax/photography/list',
 		controller(route, connect, app) {
-			ajax_photography.render(connect, app)
+			photography.render(connect, app)
 		}
 	},
 	// 用户登录认证
--- sys/router/asset.js
+++ sys/router/asset.js
@@ -1,30 +1,30 @@
 
 //图库
-let ajax_asset = require('../ajax/asset/index')
+let asset = require('../controller/api/asset/index')
 module.exports = [
 	// 静态资源
 	{
 		path: 'all /ajax/asset',
-		controller: ajax_asset.list
+		controller: asset.list
 	},
 	{
 		path: 'all /ajax/asset/upload',
-		controller: ajax_asset.upload
+		controller: asset.upload
 	},
 	{
 		path: 'all /ajax/asset/del',
-		controller: ajax_asset.deleteFile
+		controller: asset.deleteFile
 	},
 	{
 		path: 'all /ajax/asset/rename',
-		controller: ajax_asset.renamePath
+		controller: asset.renamePath
 	},
 	{
 		path: 'all /ajax/asset/delDir',
-		controller: ajax_asset.deleteDir
+		controller: asset.deleteDir
 	},
 	{
 		path: 'all /ajax/asset/createDir',
-		controller: ajax_asset.createPath
+		controller: asset.createPath
 	}
 ]
