9a8e909bce53887017ce95627ec0044fd2c72331
剧中人 <bh_lay@126.com>
2019-03-22 20:30:18 +0800

    增加 mongoclient 方法复用

--- sys/core/DB.js
+++ sys/core/DB.js
@@ -11,18 +11,6 @@ const mongoConnectUrl = `mongodb://${mongoConig.host}:${mongoConig.port}/${mongo
 const getConnect = () => {
 	var MongoClient = require('mongodb').MongoClient
 	return new Promise((resolve, reject) => {
-		MongoClient.connect(mongoConnectUrl, function (err, client) {
-			if (err) {
-				reject()
-			} else {
-				resolve(client)
-			}
-		})
-	})
-}
-const getCollection = (collectionName) => {
-	var MongoClient = require('mongodb').MongoClient
-	return new Promise((resolve, reject) => {
 		MongoClient.connect(mongoConnectUrl, {
 			useNewUrlParser: true,
 			auth: {
@@ -33,17 +21,25 @@ const getCollection = (collectionName) => {
 			if (err) {
 				reject()
 			} else {
-				var db = client.db(mongoConig.dbName)
-				const collection = db.collection(collectionName)
-				resolve({
-					collection,
-					closeDBConnect() {
-						client.close()
-					}
-				})
+				resolve(client)
 			}
 		})
 	})
+}
+
+const getCollection = (collectionName) => {
+	return getConnect()
+		.then(client => {
+			var db = client.db(mongoConig.dbName)
+			const collection = db.collection(collectionName)
+			
+			return {
+				collection,
+				closeDBConnect() {
+					client.close()
+				}
+			}
+		})
 }
 module.exports = {
 	getConnect,
