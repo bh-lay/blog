ae0b94d5a80715527c83a6afeae82eb80499a098
剧中人 <bh_lay@126.com>
2019-03-23 14:28:34 +0800

    联表计算使用 promise.all 管理

--- sys/controller/api/comments/list.js
+++ sys/controller/api/comments/list.js
@@ -2,139 +2,111 @@
 var DB = require('../../../core/DB.js')
 let showdown  = require('showdown')
 
-function getUserInfo(id,callback){
-	DB.getCollection('user')
-		.then(({collection, client}) => {
-			collection.find({
-				id: id
-			}).toArray(function(err, docs) {
-				client.close()
-				if(err || docs.length == 0){
-					callback && callback(err)
-					return
-				}
-				callback && callback(null,{
-					avatar: docs[0].avatar,
-					id: docs[0].id,
-					username: docs[0].username
-				})
-			})
-		}).catch(err => {
-			callback && callback(err)
-		})
-}
-
 /**
  * 处理评论数据
  *  增加用户信息
  *
  **/
-function handleData(docs,callback){
-	/**
-	 * 统一调用回调
-	 */
-	function endFn(){
-		// 处理用户信息字段
-		docs.forEach(function(item){
-			if(users[item.uid]){
-				item.user = users[item.uid]
-			}else if(item.user != null && typeof(item.user) == 'object'){
-				delete item.user.email
-			}else{
-				item.user = {}
-			}
-		})
-		callback&&callback(docs)
-	}
+const makeUpUserInfo = (db, docs) => {
 	
-	var users = {}
-	var uidsLength = 0
-	var overLength = 0
+	let userIDList = []
 	
-	// 获取所有需要的用户id
+	// 遍历评论列表
 	docs.forEach(item => {
-		var uid = item.uid
-		if(uid && !users[uid]){
-			users[uid] = {}
-			uidsLength++
+		// 获取所有需要的用户id
+		if(item.uid && userIDList.indexOf(item.uid) === -1){
+			userIDList.push(item.uid)
 		}
+		// 内容由 markdown 转为 html
 		var markdownConverter = new showdown.Converter()
 		item.content = markdownConverter.makeHtml(item.content)
 	})
-	if(uidsLength == 0){
-		endFn()
-	}else{
-		// 遍历所有需要的用户id
-		for(var id in users){
-			// 获取单个用户信息
-			getUserInfo(id,function(err,userInfo){
-				overLength++
-				if(!err){
-					users[id] = userInfo
-				}
-				if(overLength >= uidsLength){
-					endFn()
+	// 挨个构建获取用户信息对象
+	let userCollection = db.collection('user')
+	let getUserInfoArray = userIDList.map(uid => {
+		return new Promise((resolve) => {
+			// 获取用户信息
+			userCollection.findOne({
+				id: uid
+			}, (err, doc) => {
+				resolve((err || !doc) ? {} : {
+					avatar: doc.avatar,
+					id: doc.id,
+					username: doc.username
+				})
+			})
+		})
+	})
+	// 统一等待获取
+	return Promise.all(getUserInfoArray)
+		.then(userList => {
+			// 将获取到的用户列表转为 id 为 key 的对象
+			let users = {}
+			userList.forEach(item => {
+				users[item.id] = item
+			})
+			// 处理用户信息字段
+			docs.forEach(function(item){
+				if (users[item.uid]) {
+					item.user = users[item.uid]
+				} else {
+					item.user = item.user || {}
 				}
 			})
-		}
-	}
+
+			return docs
+		})
 }
 
 // 获取评论列表
 module.exports = function(connect,data,callback){
 	let limit_num = parseInt(data['limit']) || 10
 	let skip_num = parseInt(data['skip']) || 0
+	let getListForAdmin = !!data.isadmin
 
-	DB.getCollection('comments')
-		.then(({collection, client}) => {
-			var queryObj = {}
-			if(data['cid'] && data['cid'].length > 1){
-				queryObj.cid = data['cid']
+	DB.getDB().then(({client, db}) => {
+		const collection = db.collection('comments')
+		
+		// 按照分页获取数据
+		return DB.getDocsForPagination(collection, {
+			params: {
+				cid: data['cid']
+			},
+			limit: limit_num,
+			skip: skip_num,
+			sort: {
+				time: -1
 			}
-			collection.find(queryObj,{
-				limit:limit_num
-			}).sort({
-				time:-1
-			}).skip(skip_num).toArray(function(err, docs) {
-				if(err){
-					callback&&callback(err)
-					return
-				}
-				// count the all list
-				collection.countDocuments(queryObj,function(err,count){
+		}).then(({count, docs}) => {
+			// 是否为后台管理列表
+			if(getListForAdmin){
+				connect.session(session_this => {
+					// 用不到数据库了，关闭连接
 					client.close()
-					if(err){
-						callback&&callback(err)
+					if(session_this.get('user_group') === 'admin'){
+						callback && callback(null,{
+							count: count,
+							list: docs
+						})
 					}else{
-						// 是否为后台管理列表
-						if(data.isadmin){
-							connect.session(function(session_this){
-								if(session_this.get('user_group') == 'admin'){
-									callback&&callback(null,{
-										count: count,
-										list: docs
-									})
-								}else{
-									// 权限验证
-									callback&&callback(null,{
-										count: count,
-										list: []
-									})
-								}
-							})
-						}else{
-							// 普通列表
-							handleData(docs,function(list){
-								callback&&callback(err,{
-									'count': count,
-									'list': list
-								})
-							})
-						}
+						// 权限验证
+						callback && callback(null,{
+							count: count,
+							list: []
+						})
 					}
 				})
-			})
-		}).catch(err => {
-			callback && callback(err)
+			}else{
+				// 普通列表
+				// 补齐用户信息
+				return makeUpUserInfo(db, docs).then(list => { 
+					// 用不到数据库了，关闭连接
+					client.close()
+					callback && callback(null,{count, list})
+				})
+			}
 		})
+	}).catch(err => {
+		callback && callback(err)
+	})
 }
