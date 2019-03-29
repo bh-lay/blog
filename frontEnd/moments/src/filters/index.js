5c28df98590a4acfcdc9498dc2399bde31a83959
剧中人 <bh_lay@126.com>
2019-03-29 22:51:05 +0800

    增加相对时间计算 filter

--- frontEnd/moments/src/filters/index.js
+++ frontEnd/moments/src/filters/index.js
@@ -5,7 +5,7 @@ import Vue from 'vue'
  */
 Vue.filter('timeFormat', (time, format = '{y}-{m}-{d} {h}:{i}:{s}') => {
 	let date = new Date(parseInt(time, 10))
-	var formatObj = {
+	let formatObj = {
 		y: date.getYear() + 1900,
 		m: date.getMonth() + 1,
 		d: date.getDate(),
@@ -19,4 +19,35 @@ Vue.filter('timeFormat', (time, format = '{y}-{m}-{d} {h}:{i}:{s}') => {
 		return formatObj[b] || 0
 	})
 	return timeStr
+})
+Vue.filter('dateDiff', dateTimeStamp => {
+	let minute = 1000 * 60
+	let hour = minute * 60
+	let day = hour * 24
+	let halfamonth = day * 15
+	let month = day * 30
+	let now = new Date().getTime()
+	let diffValue = now - dateTimeStamp
+
+	let monthC = diffValue / month
+	let weekC = diffValue / (7 * day)
+	let dayC = diffValue / day
+	let hourC = diffValue / hour
+	let minC = diffValue / minute
+
+	let result
+	if (monthC >= 1) {
+		result = parseInt(monthC) + '个月前'
+	} else if (weekC >= 1) {
+		result = parseInt(weekC) + '周前'
+	} else if (dayC >= 1) {
+		result = parseInt(dayC) + '天前'
+	} else if (hourC >= 1) {
+		result = parseInt(hourC) + '个小时前'
+	} else if (minC >= 1) {
+		result = parseInt(minC) + '分钟前'
+	} else {
+		result = '刚刚'
+	}
+	return result
 })
