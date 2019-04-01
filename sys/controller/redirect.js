b102b356f66fb4a9d64410160e1c98ee7810a16a
剧中人 <bh_lay@126.com>
2019-04-01 17:47:53 +0800

    跳转功能增加访问日志功能

--- sys/controller/redirect.js
+++ sys/controller/redirect.js
@@ -2,6 +2,7 @@
  * @author bh-lay
  * 
  */
+let analysis = require('../functions/analysis/index.js')
 const base64Decode = str => {
 	/* eslint-disable-next-line no-undef */
 	return Buffer.from(str, 'base64').toString()
@@ -9,7 +10,14 @@ const base64Decode = str => {
 module.exports = function (route, connect) {
 	// 获取 URL 配置参数
 	let target = base64Decode(route.params.target || '')
-	let from = connect.request.headers.referer || ''
+	analysis.push({
+		request: connect.request,
+		response: connect.response,
+		type: 'redirect',
+		params: {
+			target
+		}
+	})
 	connect.write('define', 307, {
 		Location: target
 	}, '')
