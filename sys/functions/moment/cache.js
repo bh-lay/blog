b7353dc50d79e6265fab3ffdbce68faf50ee4ab4
剧中人 <bh_lay@126.com>
2019-04-08 18:10:39 +0800

    剧中人的朋友圈增加缓存功能

---
+++ sys/functions/moment/cache.js
@@ -0,0 +1,63 @@
+/*
+ * @author bh-lay
+ */
+let DB = require('../../core/DB.js')
+const collectionName = 'moment_cache'
+
+// 获取缓存
+const getCache = cacheName => {
+	return new Promise((resolve, reject) => {
+		DB.getCollection(collectionName)
+			.then(({collection, client}) => {
+				collection.findOne({
+					name: cacheName
+				}, (err, doc) => {
+					client.close()
+					resolve(doc)
+				})
+			}).catch(err => {
+				reject(err)
+			})
+	})
+}
+
+// 设置缓存
+const setCache = (cacheName, content) => {
+	return DB.getDB().then(({client, db}) => {
+		let collection = db.collection(collectionName)
+		// 获取缓存名下的文档总数
+		collection.countDocuments({
+			name: cacheName
+		}, (err, resultCount) => {
+			if (err) {
+				return
+			}
+			let isExist = resultCount > 0
+			if (isExist) {
+				// 存在，则更新缓存
+				collection.updateOne({
+					name: cacheName
+				}, {
+					$set: {
+						content,
+						updateTime: new Date().getTime()
+					}
+				}, () => {
+					client.close()
+				})
+			} else {
+				// 不存在，则插入一条
+				collection.insertOne({
+					name: cacheName,
+					content,
+					updateTime: new Date().getTime()
+				}, () => {
+					client.close()
+				})
+			}
+		})
+	})
+}
+
+exports.get = getCache
+exports.set = setCache
