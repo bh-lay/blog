e55161693a75612858cdeea2f6553a2261dec5ba
剧中人 <bh_lay@126.com>
2019-04-30 00:15:18 +0800

    重新整理目录创建逻辑

--- sys/controller/api/asset/createDir.js
+++ sys/controller/api/asset/createDir.js
@@ -7,33 +7,21 @@ var fs = require('fs')
 var utils = require('../../../core/utils/index.js')
 var assetPath = '../static/'
 
-module.exports = function (req, callback){
-	utils.parse.request(req, function(err, fields){
-		
-		var root = fields.pathname || ''
-		var dirName = fields.name || ''
-		
-		// 消除参数中首尾的｛/｝
-		root = root.replace(/^\/|\/$/g,'')
-		if(err || dirName.length < 1){
-			callback && callback('参数不全')
-		}else{
-			var Path = assetPath + root + '/' + dirName
-			
-			// 检测是否同名
-			var exists = fs.existsSync(Path)
-			if(exists){
-				callback && callback('目录重名')
+module.exports = function (pathname, dirName, callback){
+
+	let Path = pathname + '/' + dirName
+	
+	// 检测是否同名
+	let exists = fs.existsSync(Path)
+	if(exists){
+		callback && callback('目录重名')
+	}else{
+		fs.mkdir(Path, function(err){
+			if(err){
+				callback && callback('出错了')
 			}else{
-				fs.mkdir(Path, function(err){
-					if(err){
-						callback && callback('出错了')
-					}else{
-						callback && callback(null)
-					}
-				})
-				
+				callback && callback(null)
 			}
-		}
-	})
+		})
+	}
 }
--- sys/controller/api/asset/index.js
+++ sys/controller/api/asset/index.js
@@ -6,6 +6,7 @@ let upload = require('./upload')
 let delPath = require('./delete')
 let rename = require('./rename')
 let createDir = require('./createDir')
+var utils = require('../../../core/utils/index.js')
 let fs = require('fs')
 let assetPath = '../static/'
 
@@ -13,25 +14,12 @@ const base64Decode = str => {
 	/* eslint-disable-next-line no-undef */
 	return Buffer.from(str, 'base64').toString()
 }
-
-const ifHasPermission = (route, connect, callback) => {
-	let pathBase64 = route.params.path
-	let realPath = base64Decode(pathBase64)
-	// 去除首尾 【/】
-	realPath = realPath.replace(/^\/|\/$/g,'')
-	let pathname = assetPath + realPath
-	// 屏蔽非法请求
-	if (realPath.indexOf('../') > -1) {
-		connect.write('json',{
-			code : 201,
-			msg : 'no power'
-		})
-		return
-	}
+// 校验是否有权限操作
+const ifHasPermission = (connect, callback) => {
 	connect.session(function(session_this){
 		let userGroup = session_this.get('user_group')
 		if(userGroup === 'admin'){
-			callback && callback(pathname)
+			callback && callback()
 		}else{
 			connect.write('json',{
 				code : 201,
@@ -40,10 +28,40 @@ const ifHasPermission = (route, connect, callback) => {
 		}
 	})
 }
+// 校验路径是否合法，并返回合法路径
+const validatePath = realPath => {
+	// 屏蔽非法请求
+	if (realPath.indexOf('../') > -1) {
+		return null
+	} else {
+		return assetPath + realPath.replace(/^\/|\/$/g,'')
+	}
+}
+// 有操作权限且路径校验通过
+const ifHasPermissionAndPathValidated = (route, connect, callback) => {
+	//  从路由中获取 base64 路径参数
+	let pathBase64 = route.params.path
+	// 转换成正常路径
+	let realPath = base64Decode(pathBase64)
+	// 转换成可操作路径
+	let pathname = validatePath(realPath)
+	if (pathname) {
+		ifHasPermission(connect, () => {
+			callback && callback(pathname)
+		})
+	} else {
+		connect.write('json',{
+			code : 201,
+			msg : 'no power'
+		})
+	}
+}
+
 
 // 获取某一目录下文件（文件夹）列表
 exports.get = (route, connect) => {
-	ifHasPermission(route, connect, pathname => {
+	ifHasPermissionAndPathValidated(route, connect, pathname => {
+		
 		fs.lstat(pathname, (err, stat) => {
 			if (err) {
 				connect.write('json',{
@@ -76,7 +94,7 @@ exports.get = (route, connect) => {
 }
 // 上传文件
 exports.post = (route, connect) => {
-	ifHasPermission(route, connect, pathname => {
+	ifHasPermissionAndPathValidated(route, connect, pathname => {
 
 		upload(pathname, connect.request, (err,files) => {
 			if(err){
@@ -94,7 +112,7 @@ exports.post = (route, connect) => {
 }
 // 重命名
 exports.put = (route, connect) => {
-	ifHasPermission(route, connect, pathname => {
+	ifHasPermissionAndPathValidated(route, connect, pathname => {
 		rename(pathname, connect.request, function(err){
 			if(err){
 				connect.write('json',{
@@ -110,7 +128,7 @@ exports.put = (route, connect) => {
 }
 // 删除
 exports.delete = (route, connect) => {
-	ifHasPermission(route, connect, pathname => {
+	ifHasPermissionAndPathValidated(route, connect, pathname => {
 		delPath(pathname, connect.request, function(err){
 			if(err){
 				connect.write('json',{
@@ -127,17 +145,32 @@ exports.delete = (route, connect) => {
 }
 
 exports.createPath = (route, connect) => {
-	ifHasPermission(route, connect, () => {
-		createDir(connect.request, err => {
-			if(err){
-				connect.write('json',{
-					code : 201,
-					msg: err || '创建目录失败 ！'
-				})
-				return
-			}
+	
+	utils.parse.request(connect.request, function(err, fields){
+		var root = fields.pathname || ''
+		var dirName = fields.name || ''
+		// 转换成可操作路径
+		let pathname = validatePath(root)
+		if (!pathname) {
 			connect.write('json',{
-				code : 200
+				code : 201,
+				msg : 'no power'
+			})
+			return
+		}
+		ifHasPermission(connect, () => {
+			createDir(pathname, dirName, err => {
+				if(err){
+					connect.write('json',{
+						code : 201,
+						msg: err || '创建目录失败 ！'
+					})
+					return
+				}
+				connect.write('json',{
+					code : 200,
+					msg: 'ooooook'
+				})
 			})
 		})
 	})
