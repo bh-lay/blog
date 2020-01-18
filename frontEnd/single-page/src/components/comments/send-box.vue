f9e518b4c4465b7cd248a647796ef59816473fe0
剧中人 <bh_lay@126.com>
2020-01-18 23:26:19 +0800

    评论模块增加输入交互

--- frontEnd/single-page/src/components/comments/send-box.vue
+++ frontEnd/single-page/src/components/comments/send-box.vue
@@ -13,65 +13,84 @@
 			background #333
 	.main
 		flex-grow 1
-		.input-content
-			position relative
-			margin-bottom 15px
-			border 1px solid #ddd
-			&:before
-				content ""
-				position absolute
-				display block
-				width 14px
-				height 14px
-				top 14px
-				left -7px
-				border 1px solid #ddd
-				border-width 0 0 1px 1px
-				background #fff
-				transform rotate(45deg)
-		.placeholder
-			box-sizing border-box
+	.input-content
+		position relative
+		border 1px solid #ddd
+		&:before
+			content ""
 			position absolute
-			width 100%
-			height 100%
-			top 0
-			left 0
-			padding 15px 20px
-			line-height 20px
-			font-size 1rem
-			color #a6a6a6
-			cursor text
-			background #fff
-			small
-				font-size 13px
-				color #ddd
-		textarea
-			box-sizing border-box
 			display block
-			width 100%
-			padding 4px 10px
-			margin 0
-			border none
-			resize none
-			font-family inherit
-			line-height 24px
-			font-size 1rem
-			color #333
-			&:focus
-				outline none
-				border-color #fa0
+			width 12px
+			height 12px
+			top 15px
+			left -7px
+			border 1px solid #ddd
+			border-width 0 0 1px 1px
+			background #fff
+			transform rotate(45deg)
+	.placeholder
+		box-sizing border-box
+		position absolute
+		width 100%
+		height 100%
+		top 0
+		left 0
+		padding 15px 20px
+		line-height 20px
+		font-size 1rem
+		color #a6a6a6
+		cursor text
+		background #fff
+		transition .15s
+		small
+			font-size 13px
+			color #ddd
+	textarea
+		box-sizing border-box
+		display block
+		width 100%
+		padding 4px 10px
+		margin 0
+		border none
+		resize none
+		font-family inherit
+		line-height 24px
+		font-size 1rem
+		color #333
+		&:focus
+			outline none
+			border-color #fa0
+	.footer
+		height 0
+		overflow hidden
+		transition .2s
+		button
+			margin-top 10px
+	&.active
+		.placeholder
+			opacity 0
+			visibility hidden
+		.footer
+			height 50px
+
 </style>
 <template>
-<div class="comments-send-box">
+<div class="comments-send-box" :class="[(isStartInput || content.length) ? 'active' : 'default']">
 	<div class="side">
 		<div class="cover"></div>
 	</div>
 	<div class="main">
 		<div class="input-content">
-			<textarea name="" id="" cols="30" rows="3"></textarea>
-			<div class="placeholder">评论屌一点，BUG少一点 ！<br><small>支持 Markdown 哦 ！</small></div>
+			<textarea
+				cols="30"
+				rows="3"
+				ref="textarea"
+				@blur="isStartInput = false"
+				v-model="content"
+			></textarea>
+			<div class="placeholder" @click="startInput">评论屌一点，BUG少一点 ！<br><small>支持 Markdown 哦 ！</small></div>
 		</div>
-		<div class="footer">
+		<div class="footer" >
 			<Button type="primary">发布</Button>
 		</div>
 	</div>
@@ -85,6 +104,8 @@ export default {
 	},
 	data () {
 		return {
+			content: '',
+			isStartInput: false
 		}
 	},
 	computed: {
@@ -92,6 +113,10 @@ export default {
 	mounted () {
 	},
 	methods: {
+		startInput () {
+			this.isStartInput = true
+			this.$refs.textarea.focus()
+		}
 	}
 }
 </script>
