339b0ce174cd51e285d0de4653ef924a4e50987e
剧中人 <bh_lay@126.com>
2020-01-28 22:51:59 +0800

    增加部署方案

--- frontEnd/single-page/config/index.js
+++ frontEnd/single-page/config/index.js
@@ -53,7 +53,7 @@ module.exports = {
 
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: 'static',
+    assetsSubDirectory: '',
     assetsPublicPath: '',
 
     /**
