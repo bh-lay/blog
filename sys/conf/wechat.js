2667db8a34c2a60ef5d7b3951ea0d6ac3a50553b
剧中人 <bh_lay@126.com>
2020-03-02 21:39:43 +0800

    调整配置存放位置

--- sys/conf/wechat.js
+++ sys/conf/wechat.js
@@ -1,7 +1,6 @@
-
+/* eslint-disable no-undef */
 
 exports.weixin = {
-	localSource: 'dreamcastle',
-	AppId: 'wx9999999999',
-	AppSecret: 'ad73709c6e0815c999999999999'
+	AppId: process.env.wechatAppId,
+	AppSecret: process.env.wechatAppSecret
 }
