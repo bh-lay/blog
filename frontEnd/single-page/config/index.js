6223d34f038860b26fee5bd43b68446962171f0d
剧中人 <bh_lay@126.com>
2020-03-04 21:41:04 +0800

    Vue 版本单页项目开发模式时，支持跨终端测试

--- frontEnd/single-page/config/index.js
+++ frontEnd/single-page/config/index.js
@@ -17,7 +17,7 @@ module.exports = {
     },
 
     // Various Dev Server settings
-    host: 'localhost', // can be overwritten by process.env.HOST
+    host: '0.0.0.0', // can be overwritten by process.env.HOST
     port: 9090, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
