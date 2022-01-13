37a93e25b8f8e2688ca0d753911f08abcd1cc073
剧中人 <mail@bh-lay.com>
2022-01-14 00:10:44 +0800

    feat: 调整构建脚本

--- frontEnd/single-page/build/webpack.base.conf.js
+++ frontEnd/single-page/build/webpack.base.conf.js
@@ -31,7 +31,6 @@ module.exports = {
   output: {
     path: config.build.assetsRoot,
     filename: '[name].js',
-		publicPath: process.env.cdnDomain + '/build/single-page-vue/'
   },
   resolve: {
     extensions: ['.js', '.vue', '.json', '.styl'],
