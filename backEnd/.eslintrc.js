7232591735212f2fe478d64834f388163f00e01e
剧中人 <mail@bh-lay.com>
2023-02-26 21:48:09 +0800

    feat: 增加 Eslint 代码审查支持

--- backEnd/.eslintrc.js
+++ backEnd/.eslintrc.js
@@ -1,46 +1,9 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es6": true
-    },
-    "extends": "eslint:recommended",
-    "rules": {
-        "indent": [
-            "error",
-            "tab"
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "never"
-        ],
-        "no-console": [
-            "error",
-            {
-                allow: [
-                    "warn",
-                    "error",
-                    "log"
-                ]
-            }
-        ],
-        'spaced-comment': 1,
-        'no-eq-null': 0,
-        'no-multiple-empty-lines': 0,
-        'object-property-newline': 0,
-        'no-unused-vars': 0,
-        'no-unused-expressions': 0,
-        'no-useless-escape': 0,
-        'no-useless-return': 0,
-        'space-unary-ops': 0,
-        'no-unreachable': 0
+    parser:  '@typescript-eslint/parser',
+    extends: ['plugin:@typescript-eslint/eslint-recommended'],
+    plugins: ['@typescript-eslint'],
+    env: {
+        // browser: true,
+        node: true,
     }
-};
+}
