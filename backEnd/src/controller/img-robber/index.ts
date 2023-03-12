cf4ba8de42a865edb8417293d4fc0b72b13f45d6
剧中人 <mail@bh-lay.com>
2023-03-12 23:52:22 +0800

    fix: image robber cache error And mimes error And add cross check

--- backEnd/src/controller/img-robber/index.ts
+++ backEnd/src/controller/img-robber/index.ts
@@ -1,42 +1,72 @@
 import { promises as fs } from 'fs'
+import http from 'node:http'
 import { routeItemMatched, Connect, App } from '@/core/index'
 import downloadFile from './download-file'
 import { isFileExists, readFileToResponse } from './static-file'
 import { base64Encode, base64Decode } from '@/lib/utils'
-
 const imageRobotRoot = '/img-robber/'
-export async function get (route: routeItemMatched, connect: Connect, app: App){
+function routeSourceToRemoteData(localTemporaryRoot: string, routeSource: string) {
 	// 获取 URL 配置参数
-	const urlSourceStr = base64Decode(route.params.source as string || '')
+	const urlSourceStr = base64Decode(routeSource)
 	// 分割 URL 参数
 	const urlSourceSplit = urlSourceStr.split(/-(?=http)/)
-	// 判断是否能
+	// 判断是否合法
 	if (urlSourceSplit.length === 0) {
+		return null
+	}
+	// 获取图片原始地址和 referr 
+	const [originUrl, referrUrl] = urlSourceSplit
+	const extMatches = originUrl.match(/.\.([^.?#]+)((?:\?|#).*)?$/)
+	const ext = extMatches ? extMatches[1] : 'jpg'
+	// 生成新的文件名
+	const cacheFileName = base64Encode(originUrl).replace(/\//g, '-') + '.' + ext
+	// 生成新的文件地址
+	return {
+		cachePath: localTemporaryRoot + imageRobotRoot + cacheFileName,
+		originUrl,
+		referrUrl
+	}
+}
+function isAllowedDomain(request: http.IncomingMessage) {
+	const referer = request.headers.referer || ''
+	const domainMatches = referer.match(/^http(?:s)?\:\/\/([^\/\:]+)(?:\/|\:|$)/)
+	if (!domainMatches) {
+		return false
+	}
+	const domain = domainMatches[1]
+	if (domain === '127.0.0.1' || domain === 'localhost') {
+		return true
+	}
+	return /bh-lay\.com$/.test(domain)
+}
+export async function get(route: routeItemMatched, connect: Connect, app: App){
+	if (!isAllowedDomain(connect.request)) {
+		return connect.writeJson({
+			code: 3,
+			msg: '此接口仅限小剧自己使用哦～'
+		})
+	}
+	const routeParams = routeSourceToRemoteData(app.options.temporaryPath, route.params.source as string || '')
+	if (!routeParams) {
 		connect.writeJson({
 			code: 2,
 			msg: '获取图片失败 !'
 		})
 		return
 	}
-	
-	// 获取图片原始地址和 referr 
-	const [imageOriginUrl, imageReeferrUrl] = urlSourceSplit
-	// 生成新的文件名
-	const fileName = base64Encode(imageOriginUrl).replace(/\//g, '-')
-	// 生成新的文件地址
-	const localFilePath = app.options.temporaryPath + imageRobotRoot + fileName
+	const { cachePath, originUrl, referrUrl } = routeParams
 	// 查看文件是否存在
-	const fileExist = await isFileExists(localFilePath)
+	const fileExist = await isFileExists(cachePath)
 	
 	if (fileExist) {
 		// 文件存在，直接读取文件
-		await readFileToResponse(localFilePath, connect.request, connect.response)
+		return await readFileToResponse(cachePath, connect.request, connect.response)
 	}
 	try {
 		// 文件不存在，先下载文件
-		await downloadFile(imageOriginUrl, imageReeferrUrl, localFilePath)
+		await downloadFile(originUrl, referrUrl, cachePath)
 		// 下载成功，读取文件
-		await readFileToResponse(localFilePath, connect.request, connect.response)
+		await readFileToResponse(cachePath, connect.request, connect.response)
 	} catch(e) {
 		// 下载失败
 		connect.writeJson({
@@ -44,6 +74,6 @@ export async function get (route: routeItemMatched, connect: Connect, app: App){
 			msg: 'load error !'
 		})
 		// 下载失败，删除可能已经下载到本地的文件
-		await fs.unlink(localFilePath)
+		await fs.unlink(cachePath)
 	}
 }
