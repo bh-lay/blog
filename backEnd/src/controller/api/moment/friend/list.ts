96293771d7f42609d64778230abb35b7eedb4ff6
bh-lay <mail@bh-lay.com>
2026-01-13 23:59:56 +0800

    feat: change backend logic for friends

--- backEnd/src/controller/api/moment/friend/list.ts
+++ backEnd/src/controller/api/moment/friend/list.ts
@@ -5,7 +5,6 @@
 
 import { routeItemMatched, Connect } from '@/core/index'
 import { getDocsByPagination } from '@/database/DB'
-import power from '@/conf/power'
 
 const collectionName = 'friends'
 
@@ -13,21 +12,10 @@ export default async function (route: routeItemMatched, connect: Connect) {
   const data = connect.url.search
   const limit_num = parseInt(data.limit as string) || 10
   const skip_num = parseInt(data.skip as string) || 0
-  const params: Record<string, unknown> = {}
-  const sessionInstance = await connect.session()
-  if (!sessionInstance.power(power.BLOG_EDIT)) {
-    params.isShow = {
-      $in: [1, '1']
-    }
-  }
 
-  // 过滤标签
-  if (data.tag) {
-    params.tags = data.tag
-  }
   // 按照分页获取数据
   const {count, docs} = await getDocsByPagination(collectionName, {
-    params,
+    params: {},
     limit: limit_num,
     skip: skip_num,
     sort: {
