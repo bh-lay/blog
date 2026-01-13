ca2fdd43ed450707bd022cb1a03ddb03d1205dde
bh-lay <mail@bh-lay.com>
2026-01-13 21:57:11 +0800

    feat: add friend page to single

--- frontEnd/single-page/src/ui-library/components/container/index.vue
+++ frontEnd/single-page/src/ui-library/components/container/index.vue
@@ -1,16 +1,18 @@
 <style lang="scss" scoped>
+@use "../../../common/styles/variables" as *;
 .container {
   width: 90%;
   max-width: 1280px;
   margin: auto;
 }
-@media screen and (max-width: 600px) {
+@media screen and (max-width: $max-mobile-width) {
   .container {
     width: auto;
     margin-right: 10px;
     margin-left: 10px;
   }
 }
+
 </style>
 <template>
 <div class="container"><slot /></div>
@@ -18,10 +20,5 @@
 <script>
 export default {
 	name: 'ui-container',
-	data () {
-		return {}
-	},
-	methods: {
-	}
 }
 </script>
